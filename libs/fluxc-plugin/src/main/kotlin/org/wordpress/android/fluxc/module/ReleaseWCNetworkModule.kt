jorgecasariego/WordPress-FluxC-Android

Add basic orders store and network client

plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/module/ReleaseWCNetworkModule.kt
@@ -1,0 +1,22 @@
+package org.wordpress.android.fluxc.module
+
+import android.content.Context
+import com.android.volley.RequestQueue
+import dagger.Module
+import dagger.Provides
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderRestClient
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class ReleaseWCNetworkModule {
+    @Singleton
+    @Provides
+    fun provideWCOrderRestClient(appContext: Context, dispatcher: Dispatcher,
+                                 @Named("regular") requestQueue: RequestQueue,
+                                 token: AccessToken, userAgent: UserAgent): OrderRestClient =
+            OrderRestClient(appContext, dispatcher, requestQueue, token, userAgent)
+}

Singh-Rishabh/woocommerce-android

libs/fluxc-plugin/src/main/kotlin/org/wordpress/android/fluxc/module/ReleaseWCNetworkModule.kt
@@ -1,0 +1,22 @@
+package org.wordpress.android.fluxc.module
+
+import android.content.Context
+import com.android.volley.RequestQueue
+import dagger.Module
+import dagger.Provides
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderRestClient
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class ReleaseWCNetworkModule {
+    @Singleton
+    @Provides
+    fun provideOrderRestClient(appContext: Context, dispatcher: Dispatcher,
+                               @Named("regular") requestQueue: RequestQueue,
+                               token: AccessToken, userAgent: UserAgent): OrderRestClient =
+            OrderRestClient(appContext, dispatcher, requestQueue, token, userAgent)
+}