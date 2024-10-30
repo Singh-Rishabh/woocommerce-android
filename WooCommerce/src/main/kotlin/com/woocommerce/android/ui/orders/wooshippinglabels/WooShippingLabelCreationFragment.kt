f3ee9a1c426cbea59dc927cb8af4a8ab09035aeb
ThomazFB <thomazfcortez@gmail.com>
2024-10-30 18:18:32 -0300

    Pass the expected ViewModel to the WooShippingLabelCreationScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationFragment.kt
@@ -7,18 +7,23 @@ import android.view.ViewGroup
 import androidx.compose.material.Surface
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
 import com.woocommerce.android.ui.base.BaseFragment
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.main.AppBarStatus
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WooShippingLabelCreationFragment : BaseFragment() {
+    private val viewModel: WooShippingLabelCreationViewModel by viewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 WooThemeWithBackground {
                     Surface {
-                        WooShippingLabelCreationScreen()
+                        WooShippingLabelCreationScreen(viewModel)
                     }
                 }
             }
