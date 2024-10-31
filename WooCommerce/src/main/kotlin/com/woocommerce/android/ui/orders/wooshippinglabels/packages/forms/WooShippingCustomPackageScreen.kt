486e79b52cc5a138ddaa62e61a1cd22814f3f330
ThomazFB <thomazfcortez@gmail.com>
2024-10-31 16:55:46 -0300

    Create WooShippingCustomPackageCreationScreen scaffold

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCustomPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCustomPackageScreen.kt
@@ -1,9 +1,26 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
 
 @Composable
-fun WooShippingCustomPackageCreationScreen() {
-    Text("Custom Package Form")
+fun WooShippingCustomPackageCreationScreen(
+    modifier: Modifier = Modifier,
+    onAddPackageClick: () -> Unit
+) {
+    Column {
+        Column(modifier = modifier) {
+            Text("Package type")
+        }
+        Row(modifier = modifier) {
+            Text("Package measurements")
+        }
+        Row {
+            Text("Package Saving options")
+        }
+    }
 }
