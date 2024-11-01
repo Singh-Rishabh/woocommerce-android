db83fd6cccf4eff32145e4b86bcdc3e8c21c8be6
ThomazFB <thomazfcortez@gmail.com>
2024-11-01 10:40:34 -0300

    Fix Hilt errors

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class WooShippingLabelCreationViewModel(
+class WooShippingLabelCreationViewModel @Inject constructor(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
     fun onSelectPackageClicked() { triggerEvent(StartPackageSelection) }
