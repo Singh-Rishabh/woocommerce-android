c8e28e1de66c89e25fc6fa5768ccf10f4a9aa09e
ThomazFB <thomazfcortez@gmail.com>
2024-11-01 10:51:36 -0300

    Improve WooShippingCustomPackageCreationScreen UI scaffold placement

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCustomPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCustomPackageScreen.kt
@@ -2,12 +2,15 @@ package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
 
@@ -23,17 +26,26 @@ fun WooShippingCustomPackageCreationScreen(
     modifier: Modifier = Modifier,
     onAddPackageClick: () -> Unit
 ) {
-    Column {
-        Column(modifier = modifier) {
-            Text("Package type")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(modifier = modifier.weight(1f)) {
+            Column(modifier = modifier) {
+                Text("Package type")
+            }
+            Row(modifier = modifier) {
+                Text("Package measurements")
+            }
+            Row {
+                Text("Package Saving options")
+            }
         }
-        Row(modifier = modifier) {
-            Text("Package measurements")
-        }
-        Row {
-            Text("Package Saving options")
-        }
-        Button(onClick = onAddPackageClick) {
+        Button(
+            modifier = modifier.fillMaxWidth(),
+            onClick = onAddPackageClick
+        ) {
             Text("Add package")
         }
     }
