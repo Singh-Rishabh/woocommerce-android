afe196bc9346f24b629ff72bc63556bee758caef
AnirudhBhat <abhat38@gmail.com>
2024-11-04 09:55:38 +0530

    Refactor WooPosProductsUIEvent to hold generic WooPosItem.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsUIEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/products/WooPosProductsUIEvent.kt
@@ -1,7 +1,7 @@
 package com.woocommerce.android.ui.woopos.home.products
 
 sealed class WooPosProductsUIEvent {
-    data class ItemClicked(val item: WooPosProductsListItem) : WooPosProductsUIEvent()
+    data class ItemClicked(val item: WooPosItem) : WooPosProductsUIEvent()
     data object EndOfProductListReached : WooPosProductsUIEvent()
     data object PullToRefreshTriggered : WooPosProductsUIEvent()
     data object ProductsLoadingErrorRetryButtonClicked : WooPosProductsUIEvent()
