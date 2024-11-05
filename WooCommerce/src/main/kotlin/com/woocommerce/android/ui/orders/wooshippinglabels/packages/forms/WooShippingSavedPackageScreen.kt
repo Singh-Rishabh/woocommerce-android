bddd0b37d6345c9cb96a1d1fdc79f33e4c5db3eb
ThomazFB <thomazfcortez@gmail.com>
2024-11-05 12:16:05 -0300

    Replace WooShippingSavedPackageScreen placeholder code with actual initial implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingSavedPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingSavedPackageScreen.kt
@@ -1,9 +1,30 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
 
 @Composable
-fun WooShippingSavedPackageScreen() {
-    Text("Saved Package Form")
+fun WooShippingSavedPackageScreen(viewModel: WooShippingLabelPackageCreationViewModel) {
+    val viewState = viewModel.viewState.observeAsState()
+    WooShippingSavedPackageScreen()
+}
+
+@Composable
+fun WooShippingSavedPackageScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+    }
 }
