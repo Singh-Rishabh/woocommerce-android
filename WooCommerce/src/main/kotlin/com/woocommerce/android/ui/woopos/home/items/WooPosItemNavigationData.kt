c0c3c7417e4c6f18e4c7e494e75088d813f52fab
AnirudhBhat <abhat38@gmail.com>
2024-11-05 14:01:56 +0530

    Add navigation data for Variable products

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemNavigationData.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/WooPosItemNavigationData.kt
@@ -4,4 +4,10 @@ sealed class WooPosItemNavigationData(open val id: Long) {
     data class SimpleProductData(
         override val id: Long
     ) : WooPosItemNavigationData(id)
+
+    data class VariableProductData(
+        override val id: Long,
+        val numOfVariations: Int,
+        val variationIds: List<Long>
+    ) : WooPosItemNavigationData(id)
 }
