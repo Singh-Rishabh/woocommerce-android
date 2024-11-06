32cf5266fbf5c7e9f42709d9737e44701d997c7f
ThomazFB <thomazfcortez@gmail.com>
2024-11-06 12:18:11 -0300

    Introduce basic FetchSavedPackagesFromStore

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/FetchSavedPackagesFromStore.kt
@@ -0,0 +1,9 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.packages
+
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel.PackageData
+
+class FetchSavedPackagesFromStore {
+    operator fun invoke(): List<PackageData> {
+        return emptyList()
+    }
+}
