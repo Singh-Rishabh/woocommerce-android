a22d662eac4e9af95a96adf0aa3feb404cb81837
samiuelson <samiuelson@gmail.com>
2024-11-06 17:44:45 +0100

    Introduce `CardReaderPaymentState` model
    The goal is to make payment controller return UI-agnostic payment state and map the payment state to UI state in the target VM.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/controller/CardReaderPaymentState.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/controller/CardReaderPaymentState.kt
@@ -1,7 +1,91 @@
 package com.woocommerce.android.ui.payments.cardreader.payment.controller
 
-sealed class CardReaderPaymentState {
-    data class LoadingData(
-        val onCancelPaymentCollection: () -> Unit,
-    ) : CardReaderPaymentState()
+import androidx.annotation.StringRes
+import com.woocommerce.android.ui.payments.cardreader.payment.PaymentFlowError
+
+sealed class CardReaderPaymentOrRefundState {
+    sealed class CardReaderPaymentState: CardReaderPaymentOrRefundState() {
+        data class LoadingData(
+            val onCancelPaymentCollection: () -> Unit,
+        ) : CardReaderPaymentState()
+
+        data object ReFetchingOrder : CardReaderPaymentState()
+
+        sealed class CollectingPayment {
+            data object BuiltInReaderCollectPaymentState
+            data object ExternalReaderCollectPaymentState
+        }
+
+        sealed class ProcessingPayment {
+            data object BuiltInReaderProcessingPaymentState
+            data object ExternalReaderProcessingPaymentState
+        }
+
+        data object PrintingReceipt
+
+        sealed class PaymentCapturing(open val amountWithCurrencyLabel: String) {
+            data class BuiltInReaderPaymentCapturing(override val amountWithCurrencyLabel: String) :
+                PaymentCapturing(amountWithCurrencyLabel)
+
+            data class ExternalReaderPaymentCapturing(override val amountWithCurrencyLabel: String) :
+                PaymentCapturing(amountWithCurrencyLabel)
+        }
+
+        sealed class PaymentSuccessful: CardReaderPaymentState() {
+            data object BuiltInReaderPaymentSuccessful
+            data object ExternalReaderPaymentSuccessful
+            data object BuiltInReaderPaymentSuccessfulReceiptSentAutomatically
+            data object ExternalReaderPaymentSuccessfulReceiptSentAutomatically
+        }
+
+        sealed class PaymentFailed(
+            open val errorType: PaymentFlowError,
+            open val amountWithCurrencyLabel: String,
+            open val onCancel: () -> Unit,
+            open val onRetry: (() -> Unit)?,
+            open val cta: CallToAction? = null,
+        ): CardReaderPaymentState() {
+            data class BuiltInReaderFailedPayment(
+                override val errorType: PaymentFlowError,
+                override val amountWithCurrencyLabel: String,
+                override val onCancel: () -> Unit,
+                override val onRetry: (() -> Unit)? = null,
+                override val cta: CallToAction? = null,
+            ): PaymentFailed(
+                errorType,
+                amountWithCurrencyLabel,
+                onCancel,
+                onRetry,
+                cta,
+            )
+            data class ExternalReaderFailedPayment(
+                override val errorType: PaymentFlowError,
+                override val amountWithCurrencyLabel: String,
+                override val onCancel: () -> Unit,
+                override val onRetry: (() -> Unit)? = null,
+                override val cta: CallToAction? = null,
+            ): PaymentFailed(
+                errorType,
+                amountWithCurrencyLabel,
+                onCancel,
+                onRetry,
+                cta,
+            )
+
+            data class CallToAction(
+                @StringRes val label: Int,
+                val onCallToActionTapped: () -> Unit,
+            )
+        }
+
+        data object SharingReceipt
+    }
+
+    sealed class CardReaderRefundState {
+        data object InitializingInteracRefund
+        data object CollectingInteracRefund
+        data object ProcessingInteracRefund
+        data object InteracRefundFailure
+        data object InteracRefundSuccessful
+    }
 }
