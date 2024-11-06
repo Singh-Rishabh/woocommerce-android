cbfd0bbf3b14adf93c4673768f5f9d6702a8081e
ThomazFB <thomazfcortez@gmail.com>
2024-11-06 12:54:15 -0300

    Fix missing Dagger injection from FetchSavedPackagesFromStore

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/FetchSavedPackagesFromStore.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/FetchSavedPackagesFromStore.kt
@@ -1,8 +1,9 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages
 
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel.PackageData
+import javax.inject.Inject
 
-class FetchSavedPackagesFromStore {
+class FetchSavedPackagesFromStore @Inject constructor() {
     operator fun invoke(): List<PackageData> {
         return emptyList()
     }
