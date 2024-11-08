6ed3ebc4c56a31afc310e9e54936baabdc02f0de
ThomazFB <thomazfcortez@gmail.com>
2024-11-08 15:28:45 -0300

    Move original Package Item component out of WooShippingSavedPackageScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingSavedPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingSavedPackageScreen.kt
@@ -1,9 +1,6 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,22 +8,19 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
-import com.woocommerce.android.ui.compose.component.SelectionCheck
 import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.PackageData
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel.PackageType
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.components.WooSavedPackageListItem
 
 @Composable
 fun WooShippingSavedPackageScreen(viewModel: WooShippingLabelPackageCreationViewModel) {
@@ -59,7 +53,7 @@ fun WooShippingSavedPackageScreen(
                 .verticalScroll(rememberScrollState())
         ) {
             savedPackages.forEach { packageData ->
-                WooShippingSavedPackageItem(
+                WooSavedPackageListItem(
                     modifier,
                     packageData,
                     onSavedPackageSelected
@@ -78,46 +72,6 @@ fun WooShippingSavedPackageScreen(
                 Text(stringResource(id = R.string.woo_shipping_labels_package_creation_add_package))
             }
         }
-    }
-}
-
-@Composable
-fun WooShippingSavedPackageItem(
-    modifier: Modifier,
-    packageData: PackageData,
-    onPackageSelected: (PackageData, Boolean) -> Unit
-) {
-    Column(
-        modifier = modifier
-            .clickable { onPackageSelected(packageData, packageData.isSelected.not()) }
-            .padding(top = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            SelectionCheck(
-                isSelected = packageData.isSelected,
-                onSelectionChange = { onPackageSelected(packageData, it) }
-            )
-            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                Text(
-                    text = packageData.description,
-                    style = MaterialTheme.typography.caption,
-                    color = colorResource(id = R.color.color_on_surface_disabled)
-                )
-                Text(
-                    text = packageData.name,
-                    style = MaterialTheme.typography.body1
-                )
-                Text(
-                    text = packageData.dimensionsForDisplay,
-                    style = MaterialTheme.typography.body2
-                )
-            }
-        }
-        Divider()
     }
 }
 
