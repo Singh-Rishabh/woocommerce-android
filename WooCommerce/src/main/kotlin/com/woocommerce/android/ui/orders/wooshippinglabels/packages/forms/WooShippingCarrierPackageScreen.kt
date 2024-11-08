875f4200c6089bb3a1722f9b2f8ca899c01a3bea
ThomazFB <thomazfcortez@gmail.com>
2024-11-08 13:50:24 -0300

    Add scaffold for WooShippingCarrierPackageScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCarrierPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCarrierPackageScreen.kt
@@ -1,9 +1,38 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.Carrier
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.PackageData
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
 
 @Composable
-fun WooShippingCarrierPackageScreen() {
-    Text("Carrier Package Form")
+fun WooShippingCarrierPackageScreen(viewModel: WooShippingLabelPackageCreationViewModel) {
+    val viewState by viewModel.viewState.observeAsState()
+    viewState.apply {  }
+    WooShippingCarrierPackageScreen(
+        carrierPackages = emptyMap()
+    )
+}
+
+@Composable
+fun WooShippingCarrierPackageScreen(
+    carrierPackages: Map<Carrier, List<PackageData>>
+) {
+    Column {
+        CarrierTabRow()
+        PackageListPager()
+    }
+}
+
+@Composable
+private fun CarrierTabRow() {
+
+}
+
+@Composable
+private fun PackageListPager() {
+
 }
