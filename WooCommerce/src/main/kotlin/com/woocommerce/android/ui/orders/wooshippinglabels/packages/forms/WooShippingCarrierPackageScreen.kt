0e8ade6d2e1f8386cc36d1c3b879da969306d246
ThomazFB <thomazfcortez@gmail.com>
2024-11-08 13:57:37 -0300

    Add CarrierTabRow implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCarrierPackageScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/forms/WooShippingCarrierPackageScreen.kt
@@ -1,12 +1,33 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.forms
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.LeadingIconTab
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.Carrier
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.PackageData
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun WooShippingCarrierPackageScreen(viewModel: WooShippingLabelPackageCreationViewModel) {
@@ -17,19 +38,75 @@ fun WooShippingCarrierPackageScreen(viewModel: WooShippingLabelPackageCreationVi
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WooShippingCarrierPackageScreen(
     carrierPackages: Map<Carrier, List<PackageData>>
 ) {
+    val pagerState = rememberPagerState { carrierPackages.keys.size }
     Column {
-        CarrierTabRow()
+        CarrierTabRow(
+            pagerState = pagerState,
+            carriers = carrierPackages.keys.toList()
+        )
         PackageListPager()
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun CarrierTabRow() {
+private fun CarrierTabRow(
+    pagerState: PagerState,
+    carriers: List<Carrier>
+) {
+    val scope = rememberCoroutineScope()
+    ScrollableTabRow(
+        selectedTabIndex = pagerState.currentPage,
+        edgePadding = dimensionResource(R.dimen.major_100),
+        backgroundColor = MaterialTheme.colors.surface,
+        contentColor = MaterialTheme.colors.primary,
+    ) {
+        carriers.forEachIndexed { index, carrier ->
+            val textColor = if (index == pagerState.currentPage) {
+                MaterialTheme.colors.primary
+            } else {
+                colorResource(id = R.color.color_on_surface_medium)
+            }
+            LeadingIconTab(
+                text = {
+                    Text(
+                        text = carrier.name,
+                        color = textColor,
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                },
+                icon = {
+                    carrier.logoRes?.let { CarrierLogo(it) }
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }
+            )
+        }
+    }
+}
 
+@Composable
+private fun CarrierLogo(
+    @DrawableRes resId: Int,
+    modifier: Modifier = Modifier
+) {
+    Icon(
+        painter = painterResource(id = resId),
+        contentDescription = null,
+        modifier = modifier
+            .size(24.dp)
+            .clip(RoundedCornerShape(5.dp)),
+        tint = Color.Unspecified
+    )
 }
 
 @Composable
