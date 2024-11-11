c64077d25d6567a3c256d543bfa1d65d80347912
ThomazFB <thomazfcortez@gmail.com>
2024-11-11 16:44:42 -0300

    Define FetchCarrierPackagesFromStore

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/FetchCarrierPackagesFromStore.kt
@@ -0,0 +1,10 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
+
+import javax.inject.Inject
+
+class FetchCarrierPackagesFromStore @Inject constructor() {
+    operator fun invoke() {
+        // This is a mocked response.
+        // When fully implemented, this will be sorted from the Shipping plugin API.
+    }
+}
