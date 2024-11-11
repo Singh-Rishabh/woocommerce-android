77d714e72ad8c3ce4911c9c1406d2c1420a7f362
AnirudhBhat <abhat38@gmail.com>
2024-11-11 13:39:17 +0530

    Add unit test to verify loading state is displayed when variations view model is initiated.

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/variations/WooPosVariationsViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.woocommerce.android.ui.woopos.home.variations
+
+import com.woocommerce.android.ui.woopos.common.data.WooPosGetProductById
+import com.woocommerce.android.ui.woopos.home.items.WooPosVariationsViewState
+import com.woocommerce.android.ui.woopos.home.items.variations.WooPosVariationsDataSource
+import com.woocommerce.android.ui.woopos.home.items.variations.WooPosVariationsViewModel
+import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+@ExperimentalCoroutinesApi
+class WooPosVariationsViewModelTest {
+
+    @Rule
+    @JvmField
+    val coroutinesTestRule = WooPosCoroutineTestRule()
+
+    private val getProductById: WooPosGetProductById = mock()
+    private val variationsDataSource: WooPosVariationsDataSource = mock()
+    private lateinit var wooPosVariationsViewModel: WooPosVariationsViewModel
+
+    @Test
+    fun `given view model init, then loading state is displayed`() {
+        wooPosVariationsViewModel = WooPosVariationsViewModel(getProductById, variationsDataSource)
+
+        assertThat(
+            wooPosVariationsViewModel.viewState.value
+        ).isEqualTo(
+            WooPosVariationsViewState.Loading(withCart = true)
+        )
+    }
+}
