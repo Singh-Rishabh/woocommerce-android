8875cee944ed4d6280096933e85060336d6a1016
AnirudhBhat <abhat38@gmail.com>
2024-11-11 13:44:14 +0530

    Add unit test to verify API call is made to get product when variations view model is initiated.

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/variations/WooPosVariationsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/variations/WooPosVariationsViewModelTest.kt
@@ -6,10 +6,14 @@ import com.woocommerce.android.ui.woopos.home.items.variations.WooPosVariationsD
 import com.woocommerce.android.ui.woopos.home.items.variations.WooPosVariationsViewModel
 import com.woocommerce.android.ui.woopos.util.WooPosCoroutineTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class WooPosVariationsViewModelTest {
@@ -32,5 +36,14 @@ class WooPosVariationsViewModelTest {
         ).isEqualTo(
             WooPosVariationsViewState.Loading(withCart = true)
         )
+    }
+
+    @Test
+    fun `given view model init, then API call is made to fetch product`() = runTest {
+        wooPosVariationsViewModel = WooPosVariationsViewModel(getProductById, variationsDataSource)
+        whenever(variationsDataSource.getVariationsFlow(1L)).thenReturn(emptyFlow())
+        wooPosVariationsViewModel.init(1L)
+
+        verify(getProductById).invoke(1L)
     }
 }
