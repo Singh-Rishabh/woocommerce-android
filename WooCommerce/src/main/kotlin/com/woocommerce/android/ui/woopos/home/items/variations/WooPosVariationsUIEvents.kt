657557597f5c09c6ef18ab05f712db2f06aac69b
AnirudhBhat <abhat38@gmail.com>
2024-11-12 16:52:47 +0530

    Add event for retry button click

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsUIEvents.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsUIEvents.kt
@@ -3,4 +3,5 @@ package com.woocommerce.android.ui.woopos.home.items.variations
 sealed class WooPosVariationsUIEvents {
     data class EndOfItemsListReached(val productId: Long) : WooPosVariationsUIEvents()
     data class PullToRefreshTriggered(val productId: Long) : WooPosVariationsUIEvents()
+    data class VariationsLoadingErrorRetryButtonClicked(val productId: Long) : WooPosVariationsUIEvents()
 }
