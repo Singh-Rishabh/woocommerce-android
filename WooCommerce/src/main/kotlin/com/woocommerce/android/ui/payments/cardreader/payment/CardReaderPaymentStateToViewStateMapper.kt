3c7e8653cca9cbc0a5ef5889eb08929bb3eacaed
samiuelson <samiuelson@gmail.com>
2024-11-12 17:55:35 +0100

    Map `LoadingData`, `ReFetchingOrder`, `SharingReceipt`

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/CardReaderPaymentStateToViewStateMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/CardReaderPaymentStateToViewStateMapper.kt
@@ -1,8 +1,45 @@
 package com.woocommerce.android.ui.payments.cardreader.payment
 
 import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderInteracRefundState
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.CollectingPayment
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.LoadingData
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.PaymentCapturing
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.PaymentFailed
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.PaymentSuccessful
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.PrintingReceipt
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.ProcessingPayment
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.ReFetchingOrder
+import com.woocommerce.android.ui.payments.cardreader.payment.controller.CardReaderPaymentOrRefundState.CardReaderPaymentState.SharingReceipt
 import javax.inject.Inject
 
-class CardReaderPaymentStateToViewStateMapper @Inject constructor() {
-    operator fun invoke(): (CardReaderPaymentOrRefundState) -> ViewState = TODO()
+class CardReaderPaymentStateToViewStateMapper @Inject constructor(
+    private val cardReaderPaymentReaderTypeStateProvider: CardReaderPaymentReaderTypeStateProvider,
+) {
+    operator fun invoke(): (CardReaderPaymentOrRefundState) -> ViewState = { paymentState ->
+        when (paymentState) {
+            is CardReaderInteracRefundState.CollectingInteracRefund -> TODO()
+            is CardReaderInteracRefundState.InteracRefundFailure -> TODO()
+            is CardReaderInteracRefundState.InteracRefundSuccessful -> TODO()
+            is CardReaderInteracRefundState.LoadingData -> TODO()
+            is CardReaderInteracRefundState.ProcessingInteracRefund -> TODO()
+            is CollectingPayment.BuiltInReaderCollectPaymentState -> TODO()
+            is CollectingPayment.ExternalReaderCollectPaymentState -> TODO()
+            is LoadingData -> ViewState.LoadingDataState(paymentState.onCancel)
+            is PaymentCapturing.BuiltInReaderPaymentCapturing -> TODO()
+            is PaymentCapturing.ExternalReaderPaymentCapturing -> TODO()
+            is PaymentFailed.BuiltInReaderFailedPayment -> TODO()
+            is PaymentFailed.ExternalReaderFailedPayment -> TODO()
+            is PaymentSuccessful.BuiltInReaderPaymentSuccessful -> TODO()
+            is PaymentSuccessful.BuiltInReaderPaymentSuccessfulReceiptSentAutomatically -> TODO()
+            is PaymentSuccessful.ExternalReaderPaymentSuccessful -> TODO()
+            is PaymentSuccessful.ExternalReaderPaymentSuccessfulReceiptSentAutomatically -> TODO()
+            is PrintingReceipt -> TODO()
+            is ProcessingPayment.BuiltInReaderProcessingPayment -> TODO()
+            is ProcessingPayment.ExternalReaderProcessingPayment -> TODO()
+            ReFetchingOrder -> ViewState.ReFetchingOrderState
+            SharingReceipt -> ViewState.SharingReceiptState
+        }
+    }
 }
