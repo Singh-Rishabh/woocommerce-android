7fe29916c6cb5e08295118a318c3f6d260f0227e
samiuelson <samiuelson@gmail.com>
2024-11-14 21:09:56 +0100

    Fix tracking logic – add missing names

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/controller/CardReaderPaymentController.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/cardreader/payment/controller/CardReaderPaymentController.kt
@@ -1080,11 +1080,22 @@ class CardReaderPaymentController(
 
     private fun trackCancelledFlow(state: CardReaderPaymentOrRefundState) {
         when (state) {
-            is CardReaderPaymentState, is CardReaderInteracRefundState -> {
+            is CardReaderPaymentState -> {
                 val nameForTracking = when (state) {
                     is CardReaderPaymentState.CollectingPayment -> "Collecting"
                     is CardReaderPaymentState.PaymentCapturing -> "Capturing"
                     is CardReaderPaymentState.ProcessingPayment -> "Processing"
+                    is CardReaderPaymentState.LoadingData -> "Loading"
+                    else -> null
+                }
+                if (nameForTracking == null) {
+                    WooLog.e(WooLog.T.CARD_READER, "Invalid state received")
+                } else {
+                    tracker.trackPaymentCancelled(nameForTracking)
+                }
+            }
+            is CardReaderInteracRefundState -> {
+                val nameForTracking = when (state) {
                     is CardReaderInteracRefundState.CollectingInteracRefund -> "Collecting"
                     is CardReaderInteracRefundState.LoadingData -> "Loading"
                     is CardReaderInteracRefundState.ProcessingInteracRefund -> "Processing"
