beb6b1f899cb4c91ae4745feec923c858e786af0
AnirudhBhat <abhat38@gmail.com>
2024-11-19 08:22:18 +0530

    Use string resource for displaying number of variations

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsScreen.kt
@@ -92,7 +92,10 @@ private fun VariationsToolbar(
         )
 
         Text(
-            text = "${variableProductData.numOfVariations} variations",
+            text = stringResource(
+                id = R.string.woopos_items_list_variable_product_variations,
+                variableProductData.numOfVariations
+            ),
             style = MaterialTheme.typography.h6,
             color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
             modifier = Modifier.constrainAs(variationsCount) {
