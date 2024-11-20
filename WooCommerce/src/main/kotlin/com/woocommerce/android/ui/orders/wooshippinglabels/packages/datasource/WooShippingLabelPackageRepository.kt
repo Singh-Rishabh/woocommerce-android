334a15b77a217ebcb3b449fbadce6b75de18db7d
ThomazFB <thomazfcortez@gmail.com>
2024-11-20 16:35:47 -0300

    Introduce full package fetching function to WooShippingLabelPackageRepository

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageRepository.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageRepository.kt
@@ -1,11 +1,28 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
 
+import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking.WooShippingLabelPackageRestClient
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking.WooShippingLabelPackageRestClient.StorePackagesDTO
 import javax.inject.Inject
 import javax.inject.Singleton
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.store.WCShippingLabelStore
 
 @Singleton
 class WooShippingLabelPackageRepository @Inject constructor(
-    private val shippingLabelStore: WCShippingLabelStore
+    private val selectedSite: SelectedSite,
+    private val shippingLabelStore: WCShippingLabelStore,
+    private val packageRestClient: WooShippingLabelPackageRestClient
 ) {
+    suspend fun fetchAllStorePackages(
+        site: SiteModel = selectedSite.get()
+    ): WooResult<StorePackagesDTO> {
+        val response = packageRestClient.fetchShippingLabelPackages(site)
+
+        return response.result
+            .takeIf { response.isError.not() }
+            ?.let { WooResult(it) }
+            ?: WooResult(response.error)
+    }
 }
