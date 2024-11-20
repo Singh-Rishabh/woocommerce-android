2a89704ebb321776b8f10f641b5fd79cae5225f6
ThomazFB <thomazfcortez@gmail.com>
2024-11-20 16:13:47 -0300

    Add actual Packages GET request implementation

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/networking/WooShippingLabelPackageRestClient.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/networking/WooShippingLabelPackageRestClient.kt
@@ -1,15 +1,24 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking
 
 import javax.inject.Inject
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import org.wordpress.android.fluxc.utils.toWooPayload
 
 class WooShippingLabelPackageRestClient @Inject constructor(
     private val wooNetwork: WooNetwork
 ) {
-    private val url = "/wcshipping/v1/packages"
+    suspend fun fetchShippingLabelPackages(
+        site: SiteModel
+    ) : WooPayload<StorePackagesDTO> {
+        val url = "/wcshipping/v1/packages"
 
-    fun fetchShippingLabelPackages(): StorePackagesDTO? {
-        return null
+        return wooNetwork.executeGetGsonRequest(
+            site = site,
+            path = url,
+            clazz = StorePackagesDTO::class.java,
+        ).toWooPayload()
     }
 
     class StorePackagesDTO {
