ef0569d2630646c34f7df18294089487d0198cca
ThomazFB <thomazfcortez@gmail.com>
2024-11-21 17:05:43 -0300

    Define StorePackageDTOs.kt

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/networking/StorePackageDTOs.kt
@@ -0,0 +1,21 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking
+
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking.WooShippingLabelPackageRestClient.PredefinedPackageDTO
+
+class SavedPackageInfoDTO {
+    val custom: List<CustomPackageDTO>? = null
+    val predefined: List<PredefinedPackageDTO>? = null
+}
+
+class CustomPackageDTO {
+    val id: String? = null
+    val name: String? = null
+    val dimensions: String? = null
+    val length: Double? = null
+    val width: Double? = null
+    val height: Double? = null
+    val boxWeight: Double? = null
+    val isLetter: Boolean? = null
+    val isUserDefined: Boolean? = null
+    val type: String? = null
+}
