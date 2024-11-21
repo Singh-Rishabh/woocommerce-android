50a6a9c27fb75216603e741913045ee52843d459
ThomazFB <thomazfcortez@gmail.com>
2024-11-21 17:07:46 -0300

    Define PackageResponseDTOs.kt

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/networking/PackageResponseDTOs.kt
@@ -0,0 +1,20 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking
+
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.networking.WooShippingLabelPackageRestClient.CarrierPredefinedPackagesDTO
+
+class PackageResponse {
+    val storeOptions: PackageStoreOptionsDTO? = null
+    val packages: PackagesInfoDTO? = null
+}
+
+class PackageStoreOptionsDTO {
+    val currencySymbol: String? = null
+    val dimensionUnit: String? = null
+    val weightUnit: String? = null
+    val originCountry: String? = null
+}
+
+class PackagesInfoDTO {
+    val saved: SavedPackageInfoDTO? = null
+    val predefined: CarrierPredefinedPackagesDTO? = null
+}
