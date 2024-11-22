fe3e970799e7a98b67768b0075f62ef777fcfec3
Alejo <alejandro.torres.veiga@automattic.com>
2024-11-21 22:02:51 -0600

    add store options model

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/models/StoreOptions.kt
@@ -0,0 +1,8 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.models
+
+data class StoreOptions(
+    val currencySymbol: String,
+    val dimensionUnit: String,
+    val weightUnit: String,
+    val originCountry: String
+)
