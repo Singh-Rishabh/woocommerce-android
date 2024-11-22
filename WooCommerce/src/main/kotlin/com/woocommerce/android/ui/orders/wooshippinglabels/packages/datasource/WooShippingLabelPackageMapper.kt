5481693f693cc1c112796d23764ac6776568dac0
ThomazFB <thomazfcortez@gmail.com>
2024-11-22 17:53:13 -0300

    Add initial structure for WooShippingLabelPackageMapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
@@ -1,14 +1,34 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
 
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.CarrierGroup
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.Package
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.StorePackages
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.CarrierPredefinedPackagesDTO
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.PackageResponse
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.PredefinedPackageDTO
 import javax.inject.Inject
 
 class WooShippingLabelPackageMapper @Inject constructor() {
     operator fun invoke(response: PackageResponse): StorePackages {
+        val savedPackagesResponse = response.packages?.saved?.predefined ?: emptyList()
+
         return StorePackages(
-            savedPackages = emptyList(),
-            carrierPackages = emptyList()
+            savedPackages = mapSavedPackages(savedPackagesResponse),
+            carrierPackages = mapCarrierPackages(response.packages?.predefined)
         )
+    }
+
+    private fun mapSavedPackages(savedResponse: List<PredefinedPackageDTO>): List<Package> {
+        return savedResponse.map {
+            Package(
+                id = it.id ?: "",
+                name = it.name ?: "",
+                dimensions = it.dimensions ?: ""
+            )
+        }
+    }
+
+    private fun mapCarrierPackages(carrierPackagesResponse: CarrierPredefinedPackagesDTO?): List<CarrierGroup> {
+        return emptyList()
     }
 }
