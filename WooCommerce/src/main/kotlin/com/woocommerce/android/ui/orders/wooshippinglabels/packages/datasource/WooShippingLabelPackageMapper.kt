8f51c343ed3b9343e45a291639fd7ab72c137844
ThomazFB <thomazfcortez@gmail.com>
2024-11-26 18:54:30 -0300

    Finish CarrierGroup mapping in WooShippingLabelPackageMapper

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
@@ -3,6 +3,7 @@ package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.CarrierGroup
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.Package
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.StorePackages
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.CarrierPackageGroupDTO
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.CarrierPredefinedPackagesDTO
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.PackageResponse
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.PredefinedPackageDTO
@@ -29,6 +30,27 @@ class WooShippingLabelPackageMapper @Inject constructor() {
     }
 
     private fun mapCarrierPackages(carrierPackagesResponse: CarrierPredefinedPackagesDTO?): List<CarrierGroup> {
-        return emptyList()
+        return mutableListOf<CarrierGroup>().apply {
+            carrierPackagesResponse?.usps?.let { usps ->
+                usps.flatBoxes?.toCarrierGroup()?.let { add(it) }
+                usps.boxes?.toCarrierGroup()?.let { add(it) }
+                usps.expressBoxes?.toCarrierGroup()?.let { add(it) }
+                usps.envelopes?.toCarrierGroup()?.let { add(it) }
+            }
+            carrierPackagesResponse?.dhlExpress?.let { dhl ->
+                dhl.domesticAndInternationalPackages?.toCarrierGroup()?.let { add(it) }
+            }
+        }
     }
+
+    private fun CarrierPackageGroupDTO.toCarrierGroup() = CarrierGroup(
+        description = title.orEmpty(),
+        packages = definitions?.map {
+            Package(
+                id = it.id.orEmpty(),
+                name = it.name.orEmpty(),
+                dimensions = it.dimensions.orEmpty()
+            )
+        } ?: emptyList()
+    )
 }
