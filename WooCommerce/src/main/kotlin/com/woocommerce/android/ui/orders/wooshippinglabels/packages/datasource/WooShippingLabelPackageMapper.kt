b6fc1978776b411d6a69e622c6e4f62361ca3959
ThomazFB <thomazfcortez@gmail.com>
2024-11-27 20:26:43 -0300

    Update Mapper to account for Carrier DAO

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/WooShippingLabelPackageMapper.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
 
-import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.CarrierGroup
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.Carrier
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.CarrierPackageGroup
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.Package
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.WooShippingLabelPackageRepository.StorePackages
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.networking.CarrierPackageGroupDTO
@@ -29,21 +30,26 @@ class WooShippingLabelPackageMapper @Inject constructor() {
         }
     }
 
-    private fun mapCarrierPackages(carrierPackagesResponse: CarrierPredefinedPackagesDTO?): List<CarrierGroup> {
-        return mutableListOf<CarrierGroup>().apply {
+    private fun mapCarrierPackages(carrierPackagesResponse: CarrierPredefinedPackagesDTO?): List<Carrier> {
+        val usps = mutableListOf<CarrierPackageGroup>().apply {
             carrierPackagesResponse?.usps?.let { usps ->
                 usps.flatBoxes?.toCarrierGroup()?.let { add(it) }
                 usps.boxes?.toCarrierGroup()?.let { add(it) }
                 usps.expressBoxes?.toCarrierGroup()?.let { add(it) }
                 usps.envelopes?.toCarrierGroup()?.let { add(it) }
             }
+        }.let { Carrier(it) }
+
+        val dhl = mutableListOf<CarrierPackageGroup>().apply {
             carrierPackagesResponse?.dhlExpress?.let { dhl ->
                 dhl.domesticAndInternationalPackages?.toCarrierGroup()?.let { add(it) }
             }
-        }
+        }.let { Carrier(it) }
+
+        return listOf(usps, dhl)
     }
 
-    private fun CarrierPackageGroupDTO.toCarrierGroup() = CarrierGroup(
+    private fun CarrierPackageGroupDTO.toCarrierGroup() = CarrierPackageGroup(
         description = title.orEmpty(),
         packages = definitions?.map {
             Package(
