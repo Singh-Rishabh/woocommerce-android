64545002c904e0d4dc07037b3c9d85fb15ecffce
ThomazFB <thomazfcortez@gmail.com>
2024-11-28 17:36:48 -0300

    Introduce FetchPredefinedPackagesFromStoreTest

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/datasource/FetchPredefinedPackagesFromStoreTest.kt
@@ -0,0 +1,106 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource
+
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel.PackageType
+import com.woocommerce.android.viewmodel.BaseUnitTest
+import org.mockito.kotlin.whenever
+import org.wordpress.android.fluxc.network.BaseRequest
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.ui.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.Mockito.*
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FetchPredefinedPackagesFromStoreTest : BaseUnitTest() {
+
+    private val packageRepository: WooShippingLabelPackageRepository = mock(WooShippingLabelPackageRepository::class.java)
+    private val fetchPredefinedPackagesFromStore = FetchPredefinedPackagesFromStore(packageRepository)
+
+    @Test
+    fun `invoke should return StorePredefinedPackages with carrier and saved packages`() = testBlocking {
+        val storePackages = StorePackagesDAO(
+            savedPackages = listOf(
+                PackageDAO(
+                    id = "1",
+                    name = "Saved Package 1",
+                    dimensions = "dimensions"
+                ),
+                PackageDAO(
+                    id = "2",
+                    name = "Saved Package 2",
+                    dimensions = "dimensions"
+                )
+            ),
+            carrierPackages = mapOf(
+                CarrierType.USPS to CarrierDAO(
+                    packageGroup = listOf(
+                        CarrierPackageGroupDAO(
+                            description = "Group 1",
+                            packages = listOf(
+                                PackageDAO(
+                                    id = "1",
+                                    name = "Carrier Package 1",
+                                    dimensions = "dimensions"
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        whenever(packageRepository.fetchAllStorePackages()).thenReturn(WooResult(storePackages))
+
+        val result = fetchPredefinedPackagesFromStore()
+
+        assertThat(result.savedPackageSelection.packages).containsExactly(
+            PackageData(
+                type = PackageType.BOX,
+                name = "Saved Package 1",
+                description = "",
+                length = "",
+                width = "",
+                height = "",
+                isSelected = false
+            ),
+            PackageData(
+                type = PackageType.BOX,
+                name = "Saved Package 2",
+                description = "",
+                length = "",
+                width = "",
+                height = "",
+                isSelected = false
+            )
+        )
+        assertThat(result.carrierPackageSelection.carrierPackages[Carrier.USPS]).containsExactly(
+            CarrierPackageGroup(
+                groupName = "Group 1",
+                packages = listOf(
+                    PackageData(
+                        type = PackageType.BOX,
+                        name = "Carrier Package 1",
+                        description = "",
+                        length = "",
+                        width = "",
+                        height = "",
+                        isSelected = false
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `invoke should return empty StorePredefinedPackages when fetchAllStorePackages returns error`() = testBlocking {
+        val error = WooError(WooErrorType.GENERIC_ERROR, BaseRequest.GenericErrorType.UNKNOWN)
+        whenever(packageRepository.fetchAllStorePackages()).thenReturn(WooResult(error))
+
+        val result = fetchPredefinedPackagesFromStore()
+
+        assertThat(result.savedPackageSelection.packages).isEmpty()
+        assertThat(result.carrierPackageSelection.carrierPackages).isEmpty()
+    }
+}
