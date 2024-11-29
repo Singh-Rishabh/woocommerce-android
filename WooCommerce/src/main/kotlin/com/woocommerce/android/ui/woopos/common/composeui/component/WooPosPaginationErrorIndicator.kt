19a7ed5aa5f6b35c1cb71e84d6a6a60511292b36
AnirudhBhat <abhat38@gmail.com>
2024-11-29 09:26:07 +0530

    Refactor WooPosPaginationErrorIndicator and add content description for it.

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosPaginationErrorIndicator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosPaginationErrorIndicator.kt
@@ -2,12 +2,10 @@ package com.woocommerce.android.ui.woopos.common.composeui.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -20,9 +18,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
+import com.woocommerce.android.ui.woopos.common.composeui.ShadowType
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosCard
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
@@ -35,14 +36,12 @@ fun WooPosPaginationErrorIndicator(
     message: String,
     primaryButton: Button,
 ) {
-    WooPosCard {
-        WooPosPaginationErrorIndicatorContent(
-            modifier = modifier,
-            icon = icon,
-            message = message,
-            primaryButton = primaryButton
-        )
-    }
+    WooPosPaginationErrorIndicatorContent(
+        modifier = modifier,
+        icon = icon,
+        message = message,
+        primaryButton = primaryButton
+    )
 }
 
 @Composable
@@ -52,44 +51,48 @@ private fun WooPosPaginationErrorIndicatorContent(
     message: String,
     primaryButton: Button
 ) {
-    Row(
+    val itemContentDescription = stringResource(id = R.string.woopos_items_pagination_error_content_description)
+    WooPosCard(
         modifier = modifier
-            .fillMaxWidth()
-            .height(112.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .padding(16.dp.toAdaptivePadding()),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+            .semantics { contentDescription = itemContentDescription },
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = 6.dp,
+        shadowType = ShadowType.Soft,
     ) {
         Row(
+            modifier = Modifier
+                .height(112.dp)
+                .fillMaxWidth()
+                .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.weight(1f)
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Icon(
-                modifier = Modifier.size(24.dp),
-                painter = icon,
-                contentDescription = stringResource(R.string.woopos_error_icon_content_description),
-                tint = Color.Unspecified,
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = icon,
+                    contentDescription = stringResource(R.string.woopos_error_icon_content_description),
+                    tint = Color.Unspecified,
+                )
+                Text(
+                    text = message,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.error,
+                    modifier = Modifier.padding(start = 24.dp.toAdaptivePadding())
+                )
+            }
 
-            Spacer(modifier = Modifier.width(24.dp.toAdaptivePadding()))
-
-            Text(
-                text = message,
-                style = MaterialTheme.typography.h5,
-                fontWeight = FontWeight.SemiBold,
-                color = MaterialTheme.colors.error
-            )
-        }
-
-        Row(
-            modifier = Modifier.weight(0.5f)
-        ) {
             WooPosButton(
                 text = primaryButton.text,
                 onClick = primaryButton.click,
                 modifier = Modifier
+                    .weight(0.5f)
                     .height(50.dp)
                     .clip(RoundedCornerShape(16.dp))
             )
