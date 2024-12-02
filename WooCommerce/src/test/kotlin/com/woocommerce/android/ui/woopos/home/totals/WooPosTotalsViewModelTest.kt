e896a1367c01ad11f15f46f49a62fef376762763
samiuelson <samiuelson@gmail.com>
2024-12-02 19:09:18 +0100

    Add test:
    given reader not connected, when checkout clicked and error CTA clicked, then should try connecting to reader

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/totals/WooPosTotalsViewModelTest.kt
@@ -768,6 +768,66 @@ class WooPosTotalsViewModelTest {
         assertThat(state.error).isNull()
     }
 
+    @Test
+    fun `given reader not connected, when checkout clicked and error CTA clicked, then should try connecting to reader`() = runTest {
+        // GIVEN
+        val readerStatus: StateFlow<CardReaderStatus> =
+            MutableStateFlow<CardReaderStatus>(CardReaderStatus.NotConnected())
+        whenever(cardReaderManager.readerStatus).thenReturn(readerStatus)
+        whenever(resourceProvider.getString(R.string.woopos_success_totals_error_reader_not_connected_title))
+            .thenReturn("Reader not connected")
+        whenever(resourceProvider.getString(R.string.woopos_success_totals_error_reader_not_connected_subtitle))
+            .thenReturn("To process this payment, please connect your reader.")
+        whenever(resourceProvider.getString(R.string.woopos_success_totals_error_reader_not_connected_cta_button_label))
+            .thenReturn("Connect to reader")
+
+        val productIds = listOf(1L, 2L, 3L)
+        val order = Order.getEmptyOrder(
+            dateCreated = Date(),
+            dateModified = Date()
+        ).copy(
+            totalTax = BigDecimal("2.00"),
+            items = listOf(
+                Order.Item.EMPTY.copy(
+                    subtotal = BigDecimal("1.00"),
+                ),
+                Order.Item.EMPTY.copy(
+                    subtotal = BigDecimal("1.00"),
+                ),
+                Order.Item.EMPTY.copy(
+                    subtotal = BigDecimal("1.00"),
+                )
+            ),
+            productsTotal = BigDecimal("3.00"),
+            total = BigDecimal("5.00"),
+        )
+        val totalsRepository: WooPosTotalsRepository = mock {
+            onBlocking {
+                createOrderWithProducts(productIds = productIds)
+            }.thenReturn(Result.success(order))
+        }
+        val priceFormat: WooPosFormatPrice = mock {
+            onBlocking { invoke(BigDecimal("2.00")) }.thenReturn("2.00$")
+            onBlocking { invoke(BigDecimal("3.00")) }.thenReturn("3.00$")
+            onBlocking { invoke(BigDecimal("5.00")) }.thenReturn("5.00$")
+        }
+        val parentToChildrenEventFlow = MutableStateFlow(ParentToChildrenEvent.CheckoutClicked(productIds))
+        val parentToChildrenEventReceiver: WooPosParentToChildrenEventReceiver = mock {
+            on { events }.thenReturn(parentToChildrenEventFlow)
+        }
+
+        // WHEN
+        val viewModel = createViewModel(
+            totalsRepository = totalsRepository,
+            priceFormat = priceFormat,
+            parentToChildrenEventReceiver = parentToChildrenEventReceiver,
+        )
+        (viewModel.state.value as WooPosTotalsViewState.Totals).error!!.onAction()
+
+        // THEN
+       verify(cardReaderFacade).connectToReader()
+    }
+
     private fun createViewModel(
         resourceProvider: ResourceProvider = this.resourceProvider,
         parentToChildrenEventReceiver: WooPosParentToChildrenEventReceiver = mock(),
