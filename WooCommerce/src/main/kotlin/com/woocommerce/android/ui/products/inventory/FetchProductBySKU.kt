0e17432a816e19f5d7da86d70166a1bdbaf6a008
César Vargas Casaseca <cesar.vargas.casaseca@automattic.com>
2024-12-04 13:33:12 +0100

    Add the option to fetch product by global unique identifier

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/FetchProductBySKU.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/inventory/FetchProductBySKU.kt
@@ -15,25 +15,68 @@ class FetchProductBySKU @Inject constructor(
         codeScannerResultCode: String,
         codeScannerResultFormat: GoogleBarcodeFormatMapper.BarcodeFormat
     ): Result<Product> {
-        val product = productRepository.searchProductList(
-            searchQuery = codeScannerResultCode,
-            skuSearchOptions = WCProductStore.SkuSearchOptions.ExactSearch
-        )?.firstOrNull()
-            ?: if (codeScannerResultFormat.isEAN() || codeScannerResultFormat.isUPC()) {
-                val sku = checkDigitRemoverFactory.getCheckDigitRemoverFor(codeScannerResultFormat)
-                    .getSKUWithoutCheckDigit(codeScannerResultCode)
-                productRepository.searchProductList(
-                    searchQuery = sku,
-                    skuSearchOptions = WCProductStore.SkuSearchOptions.ExactSearch
-                )?.firstOrNull()
-            } else {
-                null
-            }
+        val product = searchProductBySku(
+            codeScannerResultCode = codeScannerResultCode,
+            codeScannerResultFormat = codeScannerResultFormat
+        ) ?: searchProductByGlobalUniqueIdentifier(
+            codeScannerResultCode = codeScannerResultCode,
+            codeScannerResultFormat = codeScannerResultFormat
+        )
+
         return if (product != null) {
             Result.success(product)
         } else {
             Result.failure(Exception("Product not found"))
         }
+    }
+
+    private suspend fun searchProductBySku(
+        codeScannerResultCode: String,
+        codeScannerResultFormat: GoogleBarcodeFormatMapper.BarcodeFormat
+    ): Product? {
+        return productRepository.searchProductList(
+            searchQuery = codeScannerResultCode,
+            skuSearchOptions = WCProductStore.SkuSearchOptions.ExactSearch
+        )?.firstOrNull()
+            ?: removeCheckDigitIfPossible(
+                codeScannerResultCode = codeScannerResultCode,
+                codeScannerResultFormat = codeScannerResultFormat
+            )?.let {
+                productRepository.searchProductList(
+                    searchQuery = it,
+                    skuSearchOptions = WCProductStore.SkuSearchOptions.ExactSearch
+                )?.firstOrNull()
+            }
+    }
+
+    private suspend fun searchProductByGlobalUniqueIdentifier(
+        codeScannerResultCode: String,
+        codeScannerResultFormat: GoogleBarcodeFormatMapper.BarcodeFormat
+    ): Product? {
+        val product = productRepository.searchProductListByGlobalUniqueId(
+            globalUniqueId = codeScannerResultCode
+        )?.firstOrNull() ?: removeCheckDigitIfPossible(
+            codeScannerResultCode = codeScannerResultCode,
+            codeScannerResultFormat = codeScannerResultFormat
+        )?.let {
+            productRepository.searchProductListByGlobalUniqueId(
+                globalUniqueId = codeScannerResultCode
+            )?.firstOrNull()
+        }
+
+        return product
+    }
+
+    private fun removeCheckDigitIfPossible(
+        codeScannerResultCode: String,
+        codeScannerResultFormat: GoogleBarcodeFormatMapper.BarcodeFormat
+    ): String? {
+        if (codeScannerResultFormat.isEAN() || codeScannerResultFormat.isUPC()) {
+            return checkDigitRemoverFactory.getCheckDigitRemoverFor(codeScannerResultFormat)
+                .getSKUWithoutCheckDigit(codeScannerResultCode)
+        }
+
+        return null
     }
 
     private fun GoogleBarcodeFormatMapper.BarcodeFormat.isUPC() =
