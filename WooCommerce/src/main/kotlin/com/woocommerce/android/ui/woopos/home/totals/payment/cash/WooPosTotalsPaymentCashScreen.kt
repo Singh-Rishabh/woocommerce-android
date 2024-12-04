67ceb727783fb13819f3889ae20df8ef22dc5634
Andrey <akidinov@gmail.com>
2024-12-04 15:51:13 +0100

    Cash payments toolbar

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/cash/WooPosTotalsPaymentCashScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/totals/payment/cash/WooPosTotalsPaymentCashScreen.kt
@@ -1,20 +1,20 @@
 package com.woocommerce.android.ui.woopos.home.totals.payment.cash
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.woocommerce.android.R
 import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
 import com.woocommerce.android.ui.woopos.common.composeui.component.WooPosButton
+import com.woocommerce.android.ui.woopos.common.composeui.toAdaptivePadding
 import com.woocommerce.android.ui.woopos.home.totals.WooPosTotalsViewState
 
 @Composable
@@ -22,16 +22,19 @@ fun WooPosTotalsPaymentCashScreen(
     state: WooPosTotalsViewState.CashPayment,
     onAmountChanged: (String) -> Unit,
     onCompleteOrderClicked: () -> Unit,
+    onBackClicked: () -> Unit,
 ) {
     Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize()
     ) {
         Column(
             modifier = Modifier
-                .width(540.dp),
+                .width(540.dp)
         ) {
+            PaymentCashToolbar(onBackClicked)
+
+            Spacer(modifier = Modifier.weight(1f))
+
             Text(
                 text = "Cash payment",
                 style = MaterialTheme.typography.h2,
@@ -70,7 +73,7 @@ fun WooPosTotalsPaymentCashScreen(
             TextField(
                 value = state.enteredAmount,
                 onValueChange = onAmountChanged,
-                label = { "Given amount" },
+                label = { Text("Given amount") },
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -80,21 +83,68 @@ fun WooPosTotalsPaymentCashScreen(
                 onClick = onCompleteOrderClicked,
                 enabled = state.canBeOrderBeCompleted,
             )
+
+            Spacer(modifier = Modifier.weight(1f))
         }
+    }
+}
+
+@Composable
+fun PaymentCashToolbar(onBackClicked: () -> Unit) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
+    ) {
+        val (backButton, title) = createRefs()
+        IconButton(
+            onClick = { onBackClicked() },
+            modifier = Modifier
+                .constrainAs(backButton) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    centerVerticallyTo(parent)
+                }
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_back_24dp),
+                contentDescription = stringResource(R.string.woopos_cart_back_content_description),
+                tint = MaterialTheme.colors.onBackground,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+
+        val iconTitlePadding = 16.dp.toAdaptivePadding()
+        Text(
+            text = stringResource(R.string.woopos_cash_payment_title),
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.onBackground,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            modifier = Modifier
+                .constrainAs(title) {
+                    top.linkTo(backButton.top)
+                    start.linkTo(backButton.end, margin = iconTitlePadding)
+                    centerVerticallyTo(parent)
+                }
+        )
     }
 }
 
 @WooPosPreview
 @Composable
 fun WooPosTotalsPaymentCashScreenScreen() {
-    WooPosTotalsPaymentCashScreen(
-        state = WooPosTotalsViewState.CashPayment(
-            enteredAmount = "5$",
-            changeDue = "5$",
-            total = "10$",
-            canBeOrderBeCompleted = true,
-        ),
-        onAmountChanged = {},
-        onCompleteOrderClicked = {},
-    )
+    WooPosTheme {
+        WooPosTotalsPaymentCashScreen(
+            state = WooPosTotalsViewState.CashPayment(
+                enteredAmount = "5$",
+                changeDue = "5$",
+                total = "10$",
+                canBeOrderBeCompleted = true,
+            ),
+            onAmountChanged = {},
+            onCompleteOrderClicked = {},
+            onBackClicked = {},
+        )
+    }
 }
