48e46337f837678fcf04fe710aae57385d583534
ThomazFB <thomazfcortez@gmail.com>
2024-12-09 17:08:56 -0300

    Add Weight data field wiring

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/WooShippingLabelPackageCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/WooShippingLabelPackageCreationViewModel.kt
@@ -152,6 +152,13 @@ class WooShippingLabelPackageCreationViewModel @Inject constructor(
         }
     }
 
+    fun onWeightChange(weight: String) {
+        _viewState.update {
+            val newPackageData = it.customPackageCreationData.copy(weight = weight)
+            it.copy(customPackageCreationData = newPackageData)
+        }
+    }
+
     fun onSavePackageChanged(checked: Boolean) {
         _viewState.update {
             val newPackageData = it.customPackageCreationData.copy(saveAsTemplate = checked)
@@ -191,7 +198,7 @@ class WooShippingLabelPackageCreationViewModel @Inject constructor(
                         innerDimensions = it.dimensions,
                         boxWeight = 0.0,
                         isUserDefined = true,
-                        maxWeight = 0.0
+                        maxWeight = it.weight.toDoubleOrNull() ?: 0.0
                     )
                 }.let { listOf(it) }
             )
