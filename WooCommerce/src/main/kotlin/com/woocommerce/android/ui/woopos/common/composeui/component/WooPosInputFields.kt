6772ff1c48830a45c8c5dd7a4dc236f39ebb033d
Andrey <akidinov@gmail.com>
2024-12-10 10:17:05 +0100

    WooPosInputField

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosInputFields.kt
@@ -0,0 +1,97 @@
+package com.woocommerce.android.ui.woopos.common.composeui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosPreview
+import com.woocommerce.android.ui.woopos.common.composeui.WooPosTheme
+
+@Composable
+fun WooPosInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String = "",
+    errorMessage: String? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = {
+                Text(
+                    text = label,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.4f),
+                )
+            },
+            isError = errorMessage != null,
+            keyboardOptions = keyboardOptions,
+            visualTransformation = visualTransformation,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+
+        if (errorMessage != null) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.caption,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            )
+        }
+    }
+}
+
+@WooPosPreview
+@Composable
+fun WooPosInputFieldPreview() {
+    WooPosTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            WooPosInputField(
+                value = "",
+                onValueChange = {},
+                label = "Label",
+            )
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            WooPosInputField(
+                value = "Text",
+                onValueChange = {},
+                label = "",
+            )
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            WooPosInputField(
+                value = "Text",
+                onValueChange = {},
+                label = "",
+                errorMessage = "Please enter a valid amount",
+            )
+        }
+    }
+}
