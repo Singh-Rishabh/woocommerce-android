182ee27375593a2f12589a719bb4165bc0fff949
ThomazFB <thomazfcortez@gmail.com>
2024-12-10 19:23:41 -0300

    Update PackageData to split dimensions into length, width and height

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/ui/UIModels.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/ui/UIModels.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.woocommerce.android.R
 import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.WooShippingLabelPackageCreationViewModel.PackageType
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -16,6 +17,20 @@ data class PackageData(
     val dimensionUnit: String = "cm",
     val weightUnit: String = "kg"
 ) : Parcelable {
+    @IgnoredOnParcel
+    val length: String
+    @IgnoredOnParcel
+    val width: String
+    @IgnoredOnParcel
+    val height: String
+
+    init {
+        val dimensionList = dimensions.split("x")
+        length = dimensionList.getOrNull(0).orEmpty().trim()
+        width = dimensionList.getOrNull(1).orEmpty().trim()
+        height = dimensionList.getOrNull(2).orEmpty().trim()
+    }
+
     val descriptionResId: Int
         get() = when (isLetter) {
             true -> R.string.woo_shipping_labels_package_creation_envelope_type
