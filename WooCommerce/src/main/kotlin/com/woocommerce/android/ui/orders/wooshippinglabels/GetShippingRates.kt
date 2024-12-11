ae1cda8d7d484bec1f88e64e2ed230061a24c121
Alejo <alejandro.torres.veiga@automattic.com>
2024-12-10 22:32:21 -0300

    add mock use case

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/GetShippingRates.kt
@@ -0,0 +1,74 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels
+
+import com.woocommerce.android.R
+import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.PackageDAO
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+import kotlin.random.Random
+
+class GetShippingRates @Inject constructor() {
+    private val cheapestComparator = Comparator<ShippingRateUI> { r1, r2 ->
+        r1.rate.substring(1).toBigDecimal().compareTo(r2.rate.substring(1).toBigDecimal())
+    }
+
+    private val fastestComparator = Comparator<ShippingRateUI> { r1, r2 ->
+        r1.deliveryDays.compareTo(r2.deliveryDays)
+    }
+
+    suspend operator fun invoke(
+        selectedPackage: PackageDAO,
+        sortOrder: ShippingSortOption
+    ): Result<Map<Carrier, List<ShippingRateUI>>> {
+        delay(1_000)
+        val comparator = when (sortOrder) {
+            ShippingSortOption.CHEAPEST -> {
+                cheapestComparator
+            }
+
+            ShippingSortOption.FASTEST -> {
+                fastestComparator
+            }
+        }
+        val carriers = if (selectedPackage.isLetter) {
+            listOf(
+                Carrier(
+                    id = "dhl",
+                    name = "DHL Express",
+                    logoRes = R.drawable.dhl_logo
+                ),
+                Carrier(
+                    id = "usps",
+                    name = "USPS",
+                    logoRes = R.drawable.usps_logo
+                )
+            )
+        } else {
+            listOf(
+                Carrier(
+                    id = "dhl",
+                    name = "DHL Express",
+                    logoRes = R.drawable.dhl_logo
+                ),
+                Carrier(
+                    id = "usps",
+                    name = "USPS",
+                    logoRes = R.drawable.usps_logo
+                ),
+                Carrier(
+                    id = "ups",
+                    name = "UPS",
+                    logoRes = R.drawable.ups_logo
+                )
+            )
+        }
+
+        return Result.success(
+            carriers.associateWith {
+                generateRates(
+                    it.name,
+                    Random(0).nextInt(from = 3, until = 10)
+                ).sortedWith(comparator)
+            }
+        )
+    }
+}
