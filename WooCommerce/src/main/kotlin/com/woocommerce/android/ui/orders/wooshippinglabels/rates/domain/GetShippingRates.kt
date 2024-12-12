08af12c24e741e20b1fbba327d54b1cc835ad777
Alejo <alejandro.torres.veiga@automattic.com>
2024-12-12 19:01:20 -0300

    get shipping rates from the repository

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/rates/domain/GetShippingRates.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/rates/domain/GetShippingRates.kt
@@ -1,33 +1,38 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.rates.domain
 
-import com.woocommerce.android.R
 import com.woocommerce.android.model.Address
 import com.woocommerce.android.ui.orders.wooshippinglabels.models.OriginShippingAddress
 import com.woocommerce.android.ui.orders.wooshippinglabels.packages.datasource.PackageDAO
-import com.woocommerce.android.ui.orders.wooshippinglabels.rates.ui.Carrier
+import com.woocommerce.android.ui.orders.wooshippinglabels.rates.datasource.WooShippingRateOptionsModel
+import com.woocommerce.android.ui.orders.wooshippinglabels.rates.datasource.WooShippingRatesRepository
+import com.woocommerce.android.ui.orders.wooshippinglabels.rates.ui.CarrierUI
 import com.woocommerce.android.ui.orders.wooshippinglabels.rates.ui.ShippingRateUI
 import com.woocommerce.android.ui.orders.wooshippinglabels.rates.ui.ShippingSortOption
-import com.woocommerce.android.ui.orders.wooshippinglabels.rates.ui.generateRates
-import kotlinx.coroutines.delay
 import javax.inject.Inject
-import kotlin.random.Random
 
-class GetShippingRates @Inject constructor() {
-    private val cheapestComparator = Comparator<ShippingRateUI> { r1, r2 ->
-        r1.rate.substring(1).toBigDecimal().compareTo(r2.rate.substring(1).toBigDecimal())
+class GetShippingRates @Inject constructor(
+    private val repository: WooShippingRatesRepository,
+    private val shippingMapper: WooShippingRatesDomainMapper
+) {
+    private val cheapestComparator = Comparator<WooShippingRateOptionsModel> { r1, r2 ->
+        r1.defaultRate.price.compareTo(r2.defaultRate.price)
     }
 
-    private val fastestComparator = Comparator<ShippingRateUI> { r1, r2 ->
-        r1.deliveryDays.compareTo(r2.deliveryDays)
+    private val fastestComparator = Comparator<WooShippingRateOptionsModel> { r1, r2 ->
+        r1.defaultRate.deliveryDays.compareTo(r2.defaultRate.deliveryDays)
     }
 
     suspend operator fun invoke(
         selectedPackage: PackageDAO,
-        sortOrder: ShippingSortOption,
         shipTo: Address,
-        shipFrom: OriginShippingAddress
-    ): Result<Map<Carrier, List<ShippingRateUI>>> {
-        delay(1_000)
+        shipFrom: OriginShippingAddress,
+        sortOrder: ShippingSortOption = ShippingSortOption.FASTEST
+    ): Result<Map<CarrierUI, List<ShippingRateUI>>> {
+        val result = repository.getShippingRates(
+            selectedPackage = selectedPackage,
+            shipTo = shipTo,
+            shipFrom = shipFrom
+        )
         val comparator = when (sortOrder) {
             ShippingSortOption.CHEAPEST -> {
                 cheapestComparator
@@ -37,46 +42,12 @@ class GetShippingRates @Inject constructor() {
                 fastestComparator
             }
         }
-        val carriers = if (selectedPackage.isLetter) {
-            listOf(
-                Carrier(
-                    id = "dhl",
-                    name = "DHL Express",
-                    logoRes = R.drawable.dhl_logo
-                ),
-                Carrier(
-                    id = "usps",
-                    name = "USPS",
-                    logoRes = R.drawable.usps_logo
-                )
-            )
-        } else {
-            listOf(
-                Carrier(
-                    id = "dhl",
-                    name = "DHL Express",
-                    logoRes = R.drawable.dhl_logo
-                ),
-                Carrier(
-                    id = "usps",
-                    name = "USPS",
-                    logoRes = R.drawable.usps_logo
-                ),
-                Carrier(
-                    id = "ups",
-                    name = "UPS",
-                    logoRes = R.drawable.ups_logo
-                )
-            )
-        }
 
-        return Result.success(
-            carriers.associateWith {
-                generateRates(
-                    it.name,
-                    Random(0).nextInt(from = 3, until = 10)
-                ).sortedWith(comparator)
-            }
-        )
+        return if (result.isSuccess) {
+            val rates = shippingMapper(result.getOrThrow())
+            Result.success(rates)
+        } else{
+            Result.failure(result.exceptionOrNull()?: Exception("Failed to get shipping rates"))
+        }
     }
 }
