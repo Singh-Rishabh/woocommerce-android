c532a5d05dc7860572207795659d9decd242e583
Andrey <akidinov@gmail.com>
2024-12-12 09:04:12 +0100

    Unit as key for toast events coroutine

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeScreen.kt
@@ -65,7 +65,7 @@ fun WooPosHomeScreen(
         }
     }
 
-    LaunchedEffect(viewModel.toastEvent) {
+    LaunchedEffect(Unit) {
         viewModel.toastEvent.collect { message ->
             ToastUtils.showToast(
                 context,
