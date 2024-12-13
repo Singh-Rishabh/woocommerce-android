63fb1c7f90b1e7cb4b41127ec8a6956c0988a6ba
ThomazFB <thomazfcortez@gmail.com>
2024-12-13 14:39:53 -0300

    Acquire App StorageDir directly instead of using a parameter

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/printing/PrintShippingLabel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/printing/PrintShippingLabel.kt
@@ -1,5 +1,7 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.printing
 
+import android.content.Context
+import android.os.Environment
 import com.woocommerce.android.media.FileUtils
 import com.woocommerce.android.util.Base64Decoder
 import com.woocommerce.android.util.CoroutineDispatchers
@@ -8,25 +10,33 @@ import javax.inject.Inject
 import kotlinx.coroutines.withContext
 
 class PrintShippingLabel @Inject constructor(
+    private val appContext: Context,
     private val dispatchers: CoroutineDispatchers,
     private val fileUtils: FileUtils,
     private val base64Decoder: Base64Decoder
 ) {
+    private val storageDir: File?
+        get() = appContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+
     suspend operator fun invoke(
-        storageDirectory: File,
         shippingLabelData: String
     ) {
         withContext(dispatchers.io) {
-            fileUtils.createTempTimeStampedFile(
-                storageDir = storageDirectory,
-                prefix = "PDF",
-                fileExtension = "pdf"
-            )?.let {
-                fileUtils.writeContentToFile(
-                    file = it,
-                    content = base64Decoder.decode(shippingLabelData, 0)
-                )
-            }
+            storageDir?.let { createShippingLabelFile(it, shippingLabelData) }
         }
+    }
+
+    private fun createShippingLabelFile(
+        storageDirectory: File,
+        shippingLabelData: String
+    ) = fileUtils.createTempTimeStampedFile(
+        storageDir = storageDirectory,
+        prefix = "PDF",
+        fileExtension = "pdf"
+    )?.let {
+        fileUtils.writeContentToFile(
+            file = it,
+            content = base64Decoder.decode(shippingLabelData, 0)
+        )
     }
 }
