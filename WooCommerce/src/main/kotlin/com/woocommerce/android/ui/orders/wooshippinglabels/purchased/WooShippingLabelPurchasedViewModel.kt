116df21d2988dfffeeb27a02d4688be8ad9015e7
ThomazFB <thomazfcortez@gmail.com>
2024-12-17 15:54:26 -0300

    Define initial WooShippingLabelPurchasedViewModel class

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/purchased/WooShippingLabelPurchasedViewModel.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.purchased
+
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WooShippingLabelPurchasedViewModel @Inject constructor(
+    savedState: SavedStateHandle
+) : ScopedViewModel(savedState) {
+    fun onPrintShippingLabelClicked() {
+
+    }
+}
