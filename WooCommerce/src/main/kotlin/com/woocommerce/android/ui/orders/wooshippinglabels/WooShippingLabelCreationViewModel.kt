7c7009f19a653f36465f53f96888737ede0c29aa
ThomazFB <thomazfcortez@gmail.com>
2024-12-17 17:21:41 -0300

    Update WooShippingLabelCreationViewModel to contain the Label ID from the purchase

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationViewModel.kt
@@ -187,8 +187,9 @@ class WooShippingLabelCreationViewModel @Inject constructor(
         triggerEvent(StartPackageSelection)
     }
 
+    // This is a arbitrary Label ID, we must replace this later with the full Shipping Labels data after the purchase
     fun onPurchaseShippingLabel() {
-        triggerEvent(LabelPurchased)
+        triggerEvent(LabelPurchased(labelId = 1234L))
     }
 
     fun onSelectedRateSortOrderChanged(option: ShippingSortOption) {
@@ -208,7 +209,7 @@ class WooShippingLabelCreationViewModel @Inject constructor(
     }
 
     data object StartPackageSelection : Event()
-    data object LabelPurchased : Event()
+    data class LabelPurchased(val labelId: Long) : Event()
 
     sealed class WooShippingViewState {
         data object Error : WooShippingViewState()
