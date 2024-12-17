ffab436501dd52181d790942ab0e934a612c7b75
ThomazFB <thomazfcortez@gmail.com>
2024-12-17 16:47:07 -0300

    Implement all WooShippingLabelPurchasedViewModel functions

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/purchased/WooShippingLabelPurchasedViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/purchased/WooShippingLabelPurchasedViewModel.kt
@@ -6,12 +6,15 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.ui.orders.wooshippinglabels.purchased.WooShippingLabelPaperSize.LETTER
 import com.woocommerce.android.ui.orders.wooshippinglabels.purchased.printing.FetchShippingLabelFile
+import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.File
 import java.util.Locale
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -35,30 +38,33 @@ class WooShippingLabelPurchasedViewModel @Inject constructor(
                         labelIds = listOf(labelId),
                         paperSize = paperSize.name.lowercase(Locale.US)
                     )
-                }
+                }?.let { triggerEvent(OpenShippingLabelFile(it)) }
         }
     }
 
     fun onLabelPaperSizeOptionSelected(paperSize: WooShippingLabelPaperSize) {
-
-
+        _viewState.update { it.copy(paperSizeOption = paperSize) }
     }
 
-    fun onTrackShipmentClicked() {
-    }
+    fun onTrackShipmentClicked() { triggerEvent(TrackShipmentRequested) }
 
-    fun onSchedulePickUpClicked() {
-    }
+    fun onSchedulePickUpClicked() { triggerEvent(SchedulePickUpRequested) }
 
-    fun onRefundClicked() {
-    }
+    fun onRefundClicked() { triggerEvent(StartRefundRequest) }
 
-    fun onLearnMoreClicked() {
-    }
+    fun onLearnMoreClicked() { triggerEvent(OpenLearnMoreScreen) }
 
     @Parcelize
     data class ViewState(
         val paperSizeOption: WooShippingLabelPaperSize? = null,
         val labelId: Long? = null
     ) : Parcelable
+
+    data class OpenShippingLabelFile(val file: File) : MultiLiveEvent.Event()
+    object TrackShipmentRequested : MultiLiveEvent.Event()
+    object SchedulePickUpRequested : MultiLiveEvent.Event()
+    object StartRefundRequest : MultiLiveEvent.Event()
+    object OpenLearnMoreScreen : MultiLiveEvent.Event()
+
+
 }
