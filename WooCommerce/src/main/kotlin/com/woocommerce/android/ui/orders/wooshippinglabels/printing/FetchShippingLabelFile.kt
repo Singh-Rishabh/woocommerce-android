62ae32f6ee4b87ee7c3d9aafa63bfba312ab4568
ThomazFB <thomazfcortez@gmail.com>
2024-12-16 23:12:24 -0300

    Update FetchShippingLabelFile to use repository instead of a direct rest client reference

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/printing/FetchShippingLabelFile.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/printing/FetchShippingLabelFile.kt
@@ -5,6 +5,7 @@ import android.os.Environment
 import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.media.FileUtils
 import com.woocommerce.android.tools.SelectedSite
+import com.woocommerce.android.ui.orders.wooshippinglabels.networking.WooShippingLabelRepository
 import com.woocommerce.android.util.Base64Decoder
 import com.woocommerce.android.util.CoroutineDispatchers
 import java.io.File
@@ -17,7 +18,7 @@ class FetchShippingLabelFile @Inject constructor(
     private val dispatchers: CoroutineDispatchers,
     private val fileUtils: FileUtils,
     private val base64Decoder: Base64Decoder,
-    private val printingRestClient: WooShippingLabelPrintingRestClient
+    private val labelRepository: WooShippingLabelRepository
 ) {
     private val storageDir: File?
         get() = appContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
@@ -26,9 +27,9 @@ class FetchShippingLabelFile @Inject constructor(
         labelIds: List<Long>
     ): File? {
         val response = selectedSite.getOrNull()
-            ?.let { printingRestClient.fetchShippingLabelPrinting(it, labelIds, "A4") }
+            ?.let { labelRepository.fetchShippingLabelPrinting(it, labelIds, "A4") }
             ?.takeIf { it.isError.not() }
-            ?.result?.b64Content
+            ?.model?.b64Content
             ?.takeIf { it.isNotNullOrEmpty() }
             ?: return null
 
