23e077c2b64504c85745e8e30ff018828fc04ea4
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-12-23 17:18:27 +0100

    Fill max size to keep the same background color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/sitevisibility/StoreVisibilityScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/sitepicker/sitevisibility/StoreVisibilityScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -91,6 +92,7 @@ fun WooSitesVisibilityScreen(
             modifier = modifier
                 .padding(padding)
                 .background(MaterialTheme.colors.surface)
+                .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .padding(horizontal = 16.dp)
         ) {
