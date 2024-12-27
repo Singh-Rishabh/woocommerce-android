69f57e6d8e8b39db736f2c6c6a117a0e4dc360c0
Alejo <alejandro.torres.veiga@automattic.com>
2024-12-27 09:26:56 -0300

    connect with real data

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/ObserveOriginAddresses.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/ObserveOriginAddresses.kt
@@ -1,65 +1,39 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.address
 
-import com.woocommerce.android.ui.orders.wooshippinglabels.models.OriginShippingAddress
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import com.woocommerce.android.ui.orders.wooshippinglabels.datasource.WooShippingAddressDataStore
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.transformLatest
 import javax.inject.Inject
 
-class ObserveOriginAddresses @Inject constructor() {
-    @Suppress("MagicNumber")
-    suspend operator fun invoke(): Flow<List<OriginShippingAddress>> {
-        delay(200)
-        val addresses = listOf(
-            OriginShippingAddress(
-                firstName = "",
-                lastName = "",
-                company = "Shut up and sip",
-                phone = "55512345",
-                address1 = "60 29TH ST PMB 343",
-                address2 = "",
-                city = "SAN FRANCISCO",
-                postcode = "94110-4929",
-                email = "alejandro.torres@mail.com",
-                country = "US",
-                state = "CA",
-                id = "store_details",
-                isDefault = true,
-                isVerified = true
-            ),
-            OriginShippingAddress(
-                firstName = "first name",
-                lastName = "last name",
-                company = "Company",
-                phone = "",
-                address1 = "Another huge address that should be truncated",
-                address2 = "",
-                city = "Oakland",
-                postcode = "",
-                email = "email",
-                country = "USA",
-                state = "California",
-                id = "id_1",
-                isDefault = false,
-                isVerified = true
-            ),
-            OriginShippingAddress(
-                firstName = "first name",
-                lastName = "last name",
-                company = "Company",
-                phone = "",
-                address1 = "Small address",
-                address2 = "",
-                city = "Palo Alto",
-                postcode = "",
-                email = "email",
-                country = "USA",
-                state = "California",
-                id = "id_1",
-                isDefault = false,
-                isVerified = true
-            )
-        )
-        return flowOf(addresses)
+class ObserveOriginAddresses @Inject constructor(
+    private val addressDataStore: WooShippingAddressDataStore,
+    private val fetchOriginAddresses: FetchOriginAddresses
+) {
+    private var isFirstValue = true
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    // We will use data store as the source of truth and after the first emission we will refresh the values async.
+    operator fun invoke() = addressDataStore.observeOriginAddresses().transformLatest { addresses ->
+        when {
+            isFirstValue && addresses == null -> {
+                // If there is no cached data, refresh the origin addresses before emitting any value
+                isFirstValue = false
+                if (fetchOriginAddresses().isFailure) {
+                    // We will use null as not available
+                    emit(null)
+                }
+            }
+
+            isFirstValue && addresses != null -> {
+                // If there is cached data, emit cached values and refresh the origin addresses async
+                isFirstValue = false
+                emit(addresses)
+                if (fetchOriginAddresses().isFailure) {
+                    emit(null)
+                }
+            }
+
+            else -> emit(addresses)
+        }
     }
 }
