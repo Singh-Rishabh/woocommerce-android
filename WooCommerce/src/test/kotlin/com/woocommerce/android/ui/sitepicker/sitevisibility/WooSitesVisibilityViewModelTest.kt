d4f33c450a36fa9c330dfd4ab6c627a1d420023e
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-12-30 12:43:44 +0100

    Add unit tests for new logic

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/sitepicker/sitevisibility/WooSitesVisibilityViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/sitepicker/sitevisibility/WooSitesVisibilityViewModelTest.kt
@@ -8,6 +8,7 @@ import com.woocommerce.android.util.getOrAwaitValue
 import com.woocommerce.android.util.runAndCaptureValues
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ExitWithResult
+import com.woocommerce.android.viewmodel.MultiLiveEvent.Event.ShowDialog
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
@@ -17,6 +18,13 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.wordpress.android.fluxc.store.NotificationStore
+import org.wordpress.android.fluxc.store.NotificationStore.NotificationSettingErrorType
+import org.wordpress.android.fluxc.store.NotificationStore.NotificationSettingsUpdateError
+import org.wordpress.android.fluxc.store.NotificationStore.SiteNotificationSetting
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class WooSitesVisibilityViewModelTest : BaseUnitTest() {
@@ -59,6 +67,8 @@ class WooSitesVisibilityViewModelTest : BaseUnitTest() {
         onBlocking { isSiteVisible(any()) } doReturn flowOf(true)
     }
 
+    private val notificationStore: NotificationStore = mock()
+
     private lateinit var viewModel: WooSitesVisibilityViewModel
 
     @Before
@@ -67,6 +77,7 @@ class WooSitesVisibilityViewModelTest : BaseUnitTest() {
             sitePickerRepository = sitePickerRepository,
             selectedSite = selectedSite,
             visibleSitesDataStore = visibleWooSitesDataStore,
+            notificationsStore = notificationStore,
             savedStateHandle = mock()
         )
     }
@@ -77,8 +88,8 @@ class WooSitesVisibilityViewModelTest : BaseUnitTest() {
             viewModel.onSiteTapped(A_WOO_SITE_UI_MODEL)
 
             val updatedState = viewModel.viewState.getOrAwaitValue()
-            assert(!updatedState.wooStores.last().isSelected)
-            assert(updatedState.isSaveButtonEnabled)
+            assertFalse(updatedState.wooStores.last().isSelected)
+            assertTrue(updatedState.isSaveButtonEnabled)
         }
 
     @Test
@@ -88,8 +99,8 @@ class WooSitesVisibilityViewModelTest : BaseUnitTest() {
             viewModel.onSiteTapped(A_WOO_SITE_UI_MODEL)
 
             val updatedState = viewModel.viewState.getOrAwaitValue()
-            assert(updatedState.wooStores.first().isSelected)
-            assert(!updatedState.isSaveButtonEnabled)
+            assertTrue(updatedState.wooStores.first().isSelected)
+            assertFalse(updatedState.isSaveButtonEnabled)
         }
 
     @Test
@@ -106,12 +117,58 @@ class WooSitesVisibilityViewModelTest : BaseUnitTest() {
         }
 
     @Test
-    fun `when tapping save, then exit with result`() =
+    fun `given updating notification settings succeeds, when tapping save, then exit with result`() =
         testBlocking {
             val event = viewModel.event.runAndCaptureValues {
                 viewModel.onSaveTapped()
             }.last()
 
             assertThat(event).isEqualTo(ExitWithResult(data = true))
+        }
+
+    @Test
+    fun `given updating notification settings fails, when tapping save, then exit with result`() =
+        testBlocking {
+            whenever(notificationStore.updateNotificationSettingsFor(any()))
+                .thenReturn(
+                    Result.failure(
+                        NotificationSettingsUpdateError(
+                            type = NotificationSettingErrorType.ApiError("Any error")
+                        )
+                    )
+                )
+
+            val event = viewModel.event.runAndCaptureValues {
+                viewModel.onSaveTapped()
+            }.last()
+
+            assertThat(event).isInstanceOf(ShowDialog::class.java)
+        }
+
+    @Test
+    fun `when tapping save, then site notification settings are updated on the backend`() =
+        testBlocking {
+
+            viewModel.onSaveTapped()
+
+            verify(notificationStore).updateNotificationSettingsFor(
+                AVAILABLE_WOO_SITES_TO_HIDE.map {
+                    SiteNotificationSetting(
+                        siteId = it.siteId,
+                        newCommentEnabled = it.isSelected,
+                        storeOrderEnabled = it.isSelected
+                    )
+                }
+            )
+        }
+
+    @Test
+    fun `given save was tapped, when loading is done, then hide loading state`() =
+        testBlocking {
+            viewModel.onSaveTapped()
+
+            val updatedState = viewModel.viewState.getOrAwaitValue()
+
+            assertFalse(updatedState.isLoading)
         }
 }
