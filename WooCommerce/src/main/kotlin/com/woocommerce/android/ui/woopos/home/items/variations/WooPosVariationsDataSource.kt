1695808a0dfd6782357d6b713e4670317d504c84
AnirudhBhat <abhat38@gmail.com>
2025-01-13 08:57:49 +0530

    Remove price filter from variations screen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsDataSource.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/variations/WooPosVariationsDataSource.kt
@@ -89,5 +89,5 @@ sealed class FetchResult {
 }
 
 private fun List<ProductVariation>.applyFilter(): List<ProductVariation> {
-    return filter { it.price != null && !it.isDownloadable }
+    return filter { !it.isDownloadable }
 }
