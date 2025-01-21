980be5d430ea91c5224a4beb2ccb6de1a7b8f9e0
ThomazFB <thomazfcortez@gmail.com>
2025-01-21 17:26:47 -0300

    Configure initial wiring between Customs form Fragment and ViewModel

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormFragment.kt
@@ -1,4 +1,10 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
-class WooShippingCustomsFormFragment {
+import androidx.fragment.app.viewModels
+import com.woocommerce.android.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WooShippingCustomsFormFragment : BaseFragment() {
+    private val viewModel: WooShippingCustomsFormViewModel by viewModels()
 }
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormViewModel.kt
@@ -1,4 +1,12 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
-class WooShippingCustomsFormViewModel {
+import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.viewmodel.ScopedViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WooShippingCustomsFormViewModel @Inject constructor(
+    savedState: SavedStateHandle
+): ScopedViewModel(savedState) {
 }
