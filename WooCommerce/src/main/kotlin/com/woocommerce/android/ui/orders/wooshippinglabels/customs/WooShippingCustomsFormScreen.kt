acc3dee384f85d6a9a5819ab7e719f5b44ffa6f9
ThomazFB <thomazfcortez@gmail.com>
2025-01-22 17:55:37 -0300

    Introduce Add Missing Information button

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormScreen.kt
@@ -2,12 +2,17 @@ package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.woocommerce.android.R
 
 @Composable
 fun WooShippingCustomsFormScreen(viewModel: WooShippingCustomsFormViewModel) {
@@ -16,7 +21,9 @@ fun WooShippingCustomsFormScreen(viewModel: WooShippingCustomsFormViewModel) {
 
 @Composable
 fun WooShippingCustomsFormScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isAddCustomsButtonEnabled: Boolean,
+    onAddCustomsDataClick: () -> Unit
 ) {
     Column(
         modifier = modifier
@@ -29,6 +36,13 @@ fun WooShippingCustomsFormScreen(
                 .verticalScroll(rememberScrollState())
         ) {
 
+        }
+        Button(
+            modifier = modifier.fillMaxWidth(),
+            enabled = isAddCustomsButtonEnabled,
+            onClick = onAddCustomsDataClick
+        ) {
+            Text(stringResource(id = R.string.woo_shipping_labels_customs_add_missing_information))
         }
     }
 }
