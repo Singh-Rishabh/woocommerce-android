f95987c1f6b4b5ca8a6b0b2026064429c1d44577
ThomazFB <thomazfcortez@gmail.com>
2025-01-22 17:53:17 -0300

    Add initial scaffold UI container for WooShippingCustomsFormScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/WooShippingCustomsFormScreen.kt
@@ -1,6 +1,13 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WooShippingCustomsFormScreen(viewModel: WooShippingCustomsFormViewModel) {
@@ -8,6 +15,20 @@ fun WooShippingCustomsFormScreen(viewModel: WooShippingCustomsFormViewModel) {
 }
 
 @Composable
-fun WooShippingCustomsFormScreen() {
+fun WooShippingCustomsFormScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
 
+        }
+    }
 }
