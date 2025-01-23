12c0a50a03865774262925f1df900ee4cd9e407f
Alejo <alejandro.torres.veiga@automattic.com>
2025-01-23 11:38:56 -0300

    add combine strings function

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/origin/WooShippingEditOriginViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/origin/WooShippingEditOriginViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.SavedStateHandle
 import com.woocommerce.android.extensions.combine
 import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.ui.orders.wooshippinglabels.address.AddressValidationHelper
+import com.woocommerce.android.util.StringUtils.combineStrings
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -125,8 +126,14 @@ class WooShippingEditOriginViewModel @Inject constructor(
     }
 
     private fun fillAddressForm() {
-        val fullName = "${navArgs.originAddress.firstName.orEmpty()} ${navArgs.originAddress.lastName.orEmpty()}"
-        val fullAddress = "${navArgs.originAddress.address1.orEmpty()} ${navArgs.originAddress.address2.orEmpty()}"
+        val fullName = combineStrings(
+            navArgs.originAddress.firstName.orEmpty(),
+            navArgs.originAddress.lastName.orEmpty()
+        )
+        val fullAddress = combineStrings(
+            navArgs.originAddress.address1.orEmpty(),
+            navArgs.originAddress.address2.orEmpty()
+        )
         name = InputValue(fullName)
         company = InputValue(navArgs.originAddress.company.orEmpty())
         country = navArgs.originAddress.country
