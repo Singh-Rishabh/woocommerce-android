3883253c8df71a781d0cba9d7e2456f37780d0ea
Hafiz Rahman <hfzrhmn@gmail.com>
2025-01-24 20:45:54 +0700

    Simplify observe logic

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ProductImagesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ProductImagesViewModel.kt
@@ -25,7 +25,6 @@ import com.woocommerce.android.viewmodel.ResourceProvider
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -218,24 +217,19 @@ class ProductImagesViewModel @Inject constructor(
             .launchIn(this)
 
         mediaFileUploadHandler.observeCurrentUploadErrors(remoteProductId)
-            .filter { it.isNotEmpty() }
-            .map {
-                viewState = viewState.copy(hasUploadErrors = true)
-                it
+            .onEach { errorList ->
+                if (errorList.isEmpty()) {
+                    viewState = viewState.copy(hasUploadErrors = false)
+                } else {
+                    viewState = viewState.copy(hasUploadErrors = true)
+                    triggerEvent(
+                        MultiLiveEvent.Event.ShowActionSnackbar(
+                            message = resourceProvider.getMediaUploadErrorMessage(errorList.size),
+                            actionText = resourceProvider.getString(R.string.details)
+                        ) { triggerEvent(ProductNavigationTarget.ViewMediaUploadErrors(remoteProductId)) }
+                    )
+                }
             }
-            .onEach {
-                triggerEvent(
-                    MultiLiveEvent.Event.ShowActionSnackbar(
-                        message = resourceProvider.getMediaUploadErrorMessage(it.size),
-                        actionText = resourceProvider.getString(R.string.details)
-                    ) { triggerEvent(ProductNavigationTarget.ViewMediaUploadErrors(remoteProductId)) }
-                )
-            }
-            .launchIn(this)
-
-        mediaFileUploadHandler.observeCurrentUploadErrors(remoteProductId)
-            .filter { it.isEmpty() }
-            .map { viewState = viewState.copy(hasUploadErrors = false) }
             .launchIn(this)
     }
 
