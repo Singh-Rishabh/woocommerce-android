7fd47ed0ed3b51fb3f1dcc155e14b94098f71ddf
ThomazFB <thomazfcortez@gmail.com>
2025-01-28 16:29:40 -0300

    Add ShouldRequireCustomsFormTest file

---
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/ShouldRequireCustomsFormTest.kt
@@ -0,0 +1,13 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.customs
+
+import org.junit.Before
+
+class ShouldRequireCustomsFormTest {
+
+    private lateinit var shouldRequireCustomsForm: ShouldRequireCustomsForm
+
+    @Before
+    fun setup() {
+        shouldRequireCustomsForm = ShouldRequireCustomsForm()
+    }
+}
