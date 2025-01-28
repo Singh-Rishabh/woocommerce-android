8b9dce04b207861f76fd40cf6c93037b1b51e8b0
ThomazFB <thomazfcortez@gmail.com>
2025-01-28 15:50:36 -0300

    Add US military state logic into ShouldRequireCustomsForm use case

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/ShouldRequireCustomsForm.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/ShouldRequireCustomsForm.kt
@@ -1,8 +1,20 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
+import com.woocommerce.android.ui.orders.wooshippinglabels.WooShippingAddresses
+
 class ShouldRequireCustomsForm {
 
-    operator fun invoke(): Boolean {
+    operator fun invoke(addressData: WooShippingAddresses): Boolean {
         return false
+    }
+
+    private fun isAddressInMilitaryState(
+        countryCode: String,
+        stateCode: String
+    ) = countryCode == US_COUNTRY_CODE && stateCode in US_MILITARY_STATES
+
+    companion object {
+        const val US_COUNTRY_CODE = "US"
+        val US_MILITARY_STATES = arrayOf("AA", "AE", "AP")
     }
 }
