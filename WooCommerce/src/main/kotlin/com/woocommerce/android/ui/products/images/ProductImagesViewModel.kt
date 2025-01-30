1d3aceaeb0122205f5677a6b9882d3669ae062da
jorgemucientesfayos <jorge.mucientes@automattic.com>
2025-01-30 08:51:05 +0100

    Remove unnecessary comment

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ProductImagesViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/images/ProductImagesViewModel.kt
@@ -77,7 +77,6 @@ class ProductImagesViewModel @Inject constructor(
     init {
         if (viewState.showSourceChooser == true) {
             viewState = viewState.copy(showSourceChooser = false)
-            // clearImageUploadErrors()
             triggerEvent(ShowImageSourceDialog)
         } else if (navArgs.selectedImage != null) {
             triggerEvent(ShowImageDetail(navArgs.selectedImage!!))
