3eb59a2c579c6e2aaff1d384cdc69c23f598e3ac
samiuelson <samiuelson@gmail.com>
2025-01-31 17:42:00 +0100

    Remove horizontal scaling

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/PaymentsBaseDialogFragment.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/payments/PaymentsBaseDialogFragment.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.DialogFragment
 import com.woocommerce.android.R
-import com.woocommerce.android.util.setWidthPercent
 
 abstract class PaymentsBaseDialogFragment(@LayoutRes val contentLayoutId: Int) : DialogFragment(contentLayoutId) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NO_TITLE, R.style.Theme_Woo_Dialog_RoundedCorners_NoMinWidth)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setWidthPercent(80)
     }
 }
