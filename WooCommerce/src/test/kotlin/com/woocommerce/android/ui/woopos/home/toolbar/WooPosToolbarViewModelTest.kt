a2b21995b64b0c9c771fc6153ba57fa1fe14ba69
AnirudhBhat <abhat38@gmail.com>
2025-02-04 13:06:40 +0530

    Add test to verify proper event and URL is emitted when clicked on POS documentation menu option

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbarViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/woopos/home/toolbar/WooPosToolbarViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.woocommerce.android.ui.woopos.home.toolbar
 
+import app.cash.turbine.test
 import com.woocommerce.android.R
 import com.woocommerce.android.cardreader.connection.CardReaderStatus
 import com.woocommerce.android.ui.woopos.cardreader.WooPosCardReaderFacade
@@ -11,8 +12,11 @@ import com.woocommerce.android.ui.woopos.util.WooPosNetworkStatus
 import com.woocommerce.android.viewmodel.ResourceProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
@@ -79,6 +83,10 @@ class WooPosToolbarViewModelTest {
             .isEqualTo(
                 WooPosToolbarState.Menu.Visible(
                     listOf(
+                        WooPosToolbarState.Menu.MenuItem(
+                            title = R.string.woopos_documentation_title,
+                            icon = R.drawable.ic_help_24dp,
+                        ),
                         WooPosToolbarState.Menu.MenuItem(
                             title = R.string.woopos_get_support_title,
                             icon = R.drawable.woopos_ic_get_support,
@@ -206,6 +214,7 @@ class WooPosToolbarViewModelTest {
         // GIVEN
         whenever(networkStatus.isConnected()).thenReturn(false)
         whenever(cardReaderFacade.readerStatus).thenReturn(MutableStateFlow(CardReaderStatus.NotConnected()))
+        whenever(resourceProvider.getString(R.string.woopos_no_internet_message)).thenReturn("No internet")
 
         // WHEN
         val viewModel = createViewModel()
@@ -213,6 +222,27 @@ class WooPosToolbarViewModelTest {
 
         // THEN
         verify(cardReaderFacade, never()).connectToReader()
+    }
+
+    @Test
+    fun `when Documentation MenuItemClicked, then openUrlEvent should be emitted with proper url`() = runTest {
+        // GIVEN
+        val viewModel = createViewModel()
+        val menuItem = WooPosToolbarState.Menu.MenuItem(
+            title = R.string.woopos_documentation_title,
+            icon = R.drawable.ic_help_24dp
+        )
+
+        viewModel.openUrlEvent.test {
+            // WHEN
+            viewModel.onUiEvent(WooPosToolbarUIEvent.MenuItemClicked(menuItem))
+
+            // THEN
+            assertEquals("https://google.com", awaitItem())
+            assertEquals(WooPosToolbarState.Menu.Hidden, viewModel.state.value.menu)
+
+            cancelAndConsumeRemainingEvents()
+        }
     }
 
     private fun createViewModel() = WooPosToolbarViewModel(
