07c8f1fcad9326171f764d15c0fd75ab9bcf6dda
samiuelson <samiuelson@gmail.com>
2025-02-17 14:59:49 +0100

    Sort events' declarations

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsEvent.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsEvent.kt
@@ -29,11 +29,11 @@ sealed class WooPosAnalyticsEvent : IAnalyticsEvent {
     }
 
     sealed class Event : WooPosAnalyticsEvent() {
-        data object ExitTapped : Event() {
-            override val name: String = "exit_menu_item_tapped"
-        }
         data object CheckoutTapped : Event() {
             override val name: String = "checkout_tapped"
+        }
+        data object ExitTapped : Event() {
+            override val name: String = "exit_menu_item_tapped"
         }
         data object ItemAddedToCart : Event() {
             override val name: String = "item_added_to_cart"
