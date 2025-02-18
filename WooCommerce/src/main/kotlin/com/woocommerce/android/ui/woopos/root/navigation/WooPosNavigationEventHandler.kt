4c5da1d131c904536a1fd300f053c3ca03d22b66
samiuelson <samiuelson@gmail.com>
2025-02-18 13:46:42 +0100

    Inject tracker to `WooPosNavigationEventHandler`

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/root/navigation/WooPosNavigationEventHandler.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/root/navigation/WooPosNavigationEventHandler.kt
@@ -2,15 +2,22 @@ package com.woocommerce.android.ui.woopos.root.navigation
 
 import androidx.activity.ComponentActivity
 import androidx.navigation.NavHostController
+import com.woocommerce.android.ui.woopos.cashpayment.CASH_ROUTE
 import com.woocommerce.android.ui.woopos.cashpayment.navigateToCashPaymentScreen
 import com.woocommerce.android.ui.woopos.emailreceipt.navigateToEmailReceipt
 import com.woocommerce.android.ui.woopos.home.navigateToHomeScreen
 import com.woocommerce.android.ui.woopos.home.navigateToHomeScreenAfterSuccessfulCashPayment
 import com.woocommerce.android.ui.woopos.home.navigateToHomeScreenIfHomeScreenNotOpen
+import com.woocommerce.android.ui.woopos.util.analytics.WooPosAnalyticsEvent.Event.BackToCheckoutFromCash
+import com.woocommerce.android.ui.woopos.util.analytics.WooPosAnalyticsTracker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 fun NavHostController.handleNavigationEvent(
     event: WooPosNavigationEvent,
     activity: ComponentActivity,
+    tracker: WooPosAnalyticsTracker,
 ) {
     when (event) {
         is WooPosNavigationEvent.ExitPosClicked,
