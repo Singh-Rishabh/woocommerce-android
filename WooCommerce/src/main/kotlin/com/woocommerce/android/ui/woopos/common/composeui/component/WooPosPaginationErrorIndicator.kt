0d641b3823aa5f6380c905419ff278ca6cf8777f
Andrey <akidinov@gmail.com>
2025-02-18 10:45:29 +0100

    Update WooPosCartScreen to use onSurfaceVariantHigh for item price color

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosPaginationErrorIndicator.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/composeui/component/WooPosPaginationErrorIndicator.kt
@@ -4,16 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -97,17 +94,18 @@ private fun WooPosPaginationErrorIndicatorContent(
                         tint = WooPosTheme.colors.unspecified,
                     )
                 }
-                Spacer(modifier = Modifier.width(WooPosSpacing.Medium.value))
                 Column {
                     Text(
                         text = message,
                         style = WooPosTypography.BodyLarge,
                         fontWeight = FontWeight.Bold,
+                        maxLines = 1,
                     )
                     Text(
                         text = description,
                         style = WooPosTypography.BodyMedium,
                         modifier = Modifier.padding(top = WooPosSpacing.Small.value.toAdaptivePadding()),
+                        maxLines = 1,
                     )
                 }
             }
@@ -117,7 +115,6 @@ private fun WooPosPaginationErrorIndicatorContent(
                 onClick = primaryButton.click,
                 modifier = Modifier
                     .padding(end = WooPosSpacing.Medium.value.toAdaptivePadding())
-                    .weight(0.25f),
             )
         }
     }
