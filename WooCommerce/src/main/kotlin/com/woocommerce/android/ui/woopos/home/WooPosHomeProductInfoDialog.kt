4c74267b2673b8570392bfc53db7f372b5c2fcde
malinajirka <malinajirka@gmail.com>
2025-02-19 14:23:17 +0100

    Replace padding with Spacer and update the spacing

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeProductInfoDialog.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/WooPosHomeProductInfoDialog.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.woocommerce.android.R
@@ -102,10 +103,10 @@ fun WooPosProductInfoDialog(
                         textAlign = TextAlign.Center,
                         modifier = Modifier.padding(bottom = WooPosSpacing.Medium.value.toAdaptivePadding())
                     )
+                    Spacer(Modifier.height(40.dp.toAdaptivePadding()))
                     Box(
                         Modifier
                             .clip(RoundedCornerShape(WooPosCornerRadius.Medium.value))
-                            .padding(vertical = WooPosSpacing.XLarge.value.toAdaptivePadding())
                             .background(
                                 color = MaterialTheme.colorScheme.surfaceDim
                             )
