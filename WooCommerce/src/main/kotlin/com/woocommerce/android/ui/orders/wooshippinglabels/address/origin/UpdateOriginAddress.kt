828181aa1133196d59543e708766d2f19a0ac367
Alejo <alejandro.torres.veiga@automattic.com>
2025-02-19 12:28:27 -0600

    change coroutine context

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/origin/UpdateOriginAddress.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/address/origin/UpdateOriginAddress.kt
@@ -4,24 +4,34 @@ import com.woocommerce.android.model.Address
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.orders.wooshippinglabels.models.OriginShippingAddress
 import com.woocommerce.android.ui.orders.wooshippinglabels.networking.WooShippingLabelRepository
+import com.woocommerce.android.util.CoroutineDispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateOriginAddress @Inject constructor(
     private val shippingRepository: WooShippingLabelRepository,
-    private val selectedSite: SelectedSite
+    private val selectedSite: SelectedSite,
+    private val coroutineDispatchers: CoroutineDispatchers
 ) {
-    suspend operator fun invoke(address: Address, addressId: String?): Result<OriginShippingAddress> {
-        return selectedSite.getOrNull()?.let {
-            val response = shippingRepository.updateOriginAddress(it, address, addressId)
-            val result = response.model
-            when {
-                response.isError || result == null -> {
-                    val message =
-                        response.error.message ?: if (result == null) "Empty response" else "Unknown error"
-                    Result.failure(Exception(message))
+    suspend operator fun invoke(
+        address: Address,
+        addressId: String?
+    ): Result<OriginShippingAddress> {
+        return withContext(coroutineDispatchers.io) {
+            selectedSite.getOrNull()?.let {
+                val response = shippingRepository.updateOriginAddress(it, address, addressId)
+                val result = response.model
+                when {
+                    response.isError || result == null -> {
+                        val message =
+                            response.error.message
+                                ?: if (result == null) "Empty response" else "Unknown error"
+                        Result.failure(Exception(message))
+                    }
+
+                    else -> Result.success(result)
                 }
-                else -> Result.success(result)
-            }
-        } ?: Result.failure(Exception("No site selected"))
+            } ?: Result.failure(Exception("No site selected"))
+        }
     }
 }
