0f861934ab2b39b36afe5bc7e7335ad0c4483424
ThomazFB <thomazfcortez@gmail.com>
2025-02-19 12:38:11 -0300

    Introduce WooShippingCustomsProductItem file

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/products/WooShippingCustomsProductItem.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.customs.products
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WooShippingCustomsProductItem() {
+
+}
+
+@Preview
+@Composable
+fun WooShippingCustomsProductItemPreview() {
+    WooShippingCustomsProductItem()
+}
