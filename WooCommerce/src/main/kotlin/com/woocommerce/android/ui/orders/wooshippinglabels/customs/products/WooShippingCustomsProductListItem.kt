5fc8562b02dec27636bf9a703723ac1b149795ee
ThomazFB <thomazfcortez@gmail.com>
2025-02-20 13:00:20 -0300

    Adjust WooShippingCustomsProductListItem spacing

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/products/WooShippingCustomsProductListItem.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/products/WooShippingCustomsProductListItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -81,6 +82,9 @@ fun WooShippingCustomsProductCollapsedListItem(
                 contentDescription = null
             )
         }
+
+        Spacer(modifier.padding(vertical = 4.dp))
+
         Row {
             Text(
                 text = itemData.description.currentInput,
