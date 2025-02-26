d3003a4c313efd703905376c290eaa8a6617a656
samiuelson <samiuelson@gmail.com>
2025-02-26 20:15:01 +0100

    Make timestamps nullable

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsDataKeeper.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/util/analytics/WooPosAnalyticsDataKeeper.kt
@@ -1,25 +1,22 @@
 package com.woocommerce.android.ui.woopos.util.analytics
 
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WooPosAnalyticsTrackingDataKeeper(
-    var interactionWithCustomerStartedTimestamp: Long = EMPTY_TIMESTAMP,
-    var orderSyncSuccessTimestamp: Long = EMPTY_TIMESTAMP,
-    var readerReadyForPaymentTimestamp: Long = EMPTY_TIMESTAMP,
-    var cardTappedTimestamp: Long = EMPTY_TIMESTAMP,
-    var checkoutButtonTapsCount: Int = 0,
-) {
+class WooPosAnalyticsTrackingDataKeeper @Inject constructor() {
+    var interactionWithCustomerStartedTimestamp: Long? = null
+    var orderSyncSuccessTimestamp: Long? = null
+    var readerReadyForPaymentTimestamp: Long? = null
+    var cardTappedTimestamp: Long? = null
+    var checkoutButtonTapsCount: Int = 0
+
 
     fun reset() {
-        interactionWithCustomerStartedTimestamp = EMPTY_TIMESTAMP
-        orderSyncSuccessTimestamp = EMPTY_TIMESTAMP
-        readerReadyForPaymentTimestamp = EMPTY_TIMESTAMP
-        cardTappedTimestamp = EMPTY_TIMESTAMP
+        interactionWithCustomerStartedTimestamp = null
+        orderSyncSuccessTimestamp = null
+        readerReadyForPaymentTimestamp = null
+        cardTappedTimestamp = null
         checkoutButtonTapsCount = 0
-    }
-
-    private companion object  {
-        private const val EMPTY_TIMESTAMP = -1L
     }
 }
