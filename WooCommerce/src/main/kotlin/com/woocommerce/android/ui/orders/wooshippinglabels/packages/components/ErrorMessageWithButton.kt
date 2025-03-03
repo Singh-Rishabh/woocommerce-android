5f1d67509e4c53a68683ea39bd61322b6b63a729
Irfan Ömür <omur.irfan@gmail.com>
2025-03-04 02:52:34 +0300

    Refactor ErrorMessageWithButton to center error message text

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/components/ErrorMessageWithButton.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/packages/components/ErrorMessageWithButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
@@ -41,6 +42,7 @@ fun ErrorMessageWithButton(
         Text(
             text = stringResource(message),
             style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
             color = MaterialTheme.colors.onSurface,
         )
         WCTextButton(onClick = onRetryClick) {
