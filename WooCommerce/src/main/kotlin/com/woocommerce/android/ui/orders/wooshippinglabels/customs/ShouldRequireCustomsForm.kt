1ecb93c24f578d339f69394ad58eb837f6dbc9c5
Alejo <alejandro.torres.veiga@automattic.com>
2025-03-04 16:57:46 -0600

    require customs only when necessary

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/ShouldRequireCustomsForm.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/ShouldRequireCustomsForm.kt
@@ -1,18 +1,19 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs
 
-import com.woocommerce.android.extensions.isNotNullOrEmpty
 import com.woocommerce.android.ui.orders.wooshippinglabels.WooShippingAddresses
 import javax.inject.Inject
 
 class ShouldRequireCustomsForm @Inject constructor() {
     operator fun invoke(addressData: WooShippingAddresses): Boolean {
-        if (addressData.isDifferentCountryShipment) return true
+        val isCountryEmpty = addressData.shipTo.address.country.code.isEmpty()
+        val isDifferentCountry = addressData.isDifferentCountryShipment
+
+        if (isCountryEmpty.not() && isDifferentCountry) return true
 
         val isOriginAddressMilitary = isAddressInMilitaryState(
             addressData.shipFrom.country,
             addressData.shipFrom.state.orEmpty()
         )
-
         val isShippingAddressMilitary = isAddressInMilitaryState(
             addressData.shipTo.address.country.code,
             addressData.shipTo.address.state.codeOrRaw
@@ -22,7 +23,7 @@ class ShouldRequireCustomsForm @Inject constructor() {
     }
 
     private val WooShippingAddresses.isDifferentCountryShipment
-        get() = shipTo.address.country.code.isNotNullOrEmpty() && shipFrom.country != shipTo.address.country.code
+        get() = shipFrom.country != shipTo.address.country.code
 
     private fun isAddressInMilitaryState(
         countryCode: String,
