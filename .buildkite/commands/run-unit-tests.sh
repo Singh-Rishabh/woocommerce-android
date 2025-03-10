6f27d4e0e865d533a040fc59b5a193ddbdd8aed0
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-03-10 20:48:28 +0100

    Revert "Run unit tests against `wasabi` variant"
    This reverts commit 9fbb1910d9766f7a8b527f4fceda121bca8168c4.

--- .buildkite/commands/run-unit-tests.sh
+++ .buildkite/commands/run-unit-tests.sh
@@ -10,7 +10,7 @@ bundle exec fastlane run configure_apply
 
 echo "--- 🧪 Testing"
 set +e
-./gradlew testWasabiDebugUnitTest testDebugUnitTest
+./gradlew testJalapenoDebugUnitTest testDebugUnitTest
 TESTS_EXIT_STATUS=$?
 set -e
 
@@ -25,7 +25,7 @@ echo "--- 🚦 Report Tests Status"
 results_file="WooCommerce/build/test-results/merged-test-results.xml"
 # Merge JUnit results into a single file (for performance reasons with reporting)
 # See https://github.com/woocommerce/woocommerce-android/pull/12064
-merge_junit_reports -d WooCommerce/build/test-results/testWasabiDebugUnitTest -o "$results_file"
+merge_junit_reports -d WooCommerce/build/test-results/testJalapenoDebugUnitTest -o "$results_file"
 
 if [[ $BUILDKITE_BRANCH == trunk ]] || [[ $BUILDKITE_BRANCH == release/* ]]; then
     annotate_test_failures "$results_file" --slack "build-and-ship"
