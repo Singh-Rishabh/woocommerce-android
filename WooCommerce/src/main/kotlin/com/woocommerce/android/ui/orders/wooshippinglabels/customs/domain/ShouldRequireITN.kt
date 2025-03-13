b9ee4f166b793ae99868cafdf3b1a493524dd799
ThomazFB <thomazfcortez@gmail.com>
2025-03-13 09:08:58 -0300

    Add Exempt and Required Countries to ShouldRequireITN

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/domain/ShouldRequireITN.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/customs/domain/ShouldRequireITN.kt
@@ -1,11 +1,15 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.customs.domain
 
 import javax.inject.Inject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 class ShouldRequireITN @Inject constructor() {
-    operator fun invoke(): Flow<Boolean> {
-        return flowOf(false)
+    operator fun invoke(
+        destinationCountryCode: String,
+        totalShippingItemValue: Float
+    ): Boolean {
+        return false
     }
+
+    private val itnExemptCountries = listOf("CA")
+    private val itnRequiredCountries = listOf("IR", "KP", "SY", "CU", "SD")
 }
