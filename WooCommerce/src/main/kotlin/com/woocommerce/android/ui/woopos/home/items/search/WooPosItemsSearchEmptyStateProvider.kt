59f5a373bc73617106e316697b957c1bc2b0bc19
Andrey <akidinov@gmail.com>
2025-03-14 08:25:28 +0100

    Add WooPosItemsSearchEmptyStateProvider and integrate with ViewModel

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchEmptyStateProvider.kt
@@ -0,0 +1,41 @@
+package com.woocommerce.android.ui.woopos.home.items.search
+
+import com.woocommerce.android.ui.woopos.home.items.WooPosItem
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class WooPosItemsSearchEmptyStateProvider @Inject constructor() {
+    suspend fun getPopularItems(): List<WooPosItem> {
+        delay(50)
+        return listOf<WooPosItem>(
+            WooPosItem.SimpleProduct(
+                id = 1,
+                name = "Popular Item 1",
+                price = "10.0$",
+                imageUrl = "https://example.com/image1.jpg",
+            ),
+            WooPosItem.SimpleProduct(
+                id = 2,
+                name = "Popular Item 2",
+                price = "20.0$",
+                imageUrl = "https://example.com/image2.jpg",
+            ),
+            WooPosItem.Variation(
+                id = 3,
+                name = "Popular Item 3",
+                productId = 1,
+                price = "30.0$",
+                imageUrl = "https://example.com/image3.jpg",
+            ),
+        )
+    }
+
+    suspend fun getLastSearches(): List<String> {
+        delay(70)
+        return listOf(
+            "T-shirt",
+            "Jeans",
+            "Shoes",
+        )
+    }
+}
--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchViewModel.kt
@@ -3,14 +3,18 @@ package com.woocommerce.android.ui.woopos.home.items.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class WooPosItemsSearchViewModel @Inject constructor() : ViewModel() {
+class WooPosItemsSearchViewModel @Inject constructor(
+    val emptyStateProvider: WooPosItemsSearchEmptyStateProvider,
+) : ViewModel() {
     private val _viewState =
         MutableStateFlow<WooPosItemsSearchViewState>(WooPosItemsSearchViewState.Empty)
     val viewState: StateFlow<WooPosItemsSearchViewState> = _viewState
@@ -19,4 +23,16 @@ class WooPosItemsSearchViewModel @Inject constructor() : ViewModel() {
             started = SharingStarted.WhileSubscribed(),
             initialValue = _viewState.value,
         )
+
+    init {
+        viewModelScope.launch {
+            val lastSearchesDeferred = async { emptyStateProvider.getLastSearches() }
+            val popularItemsDeferred = async { emptyStateProvider.getPopularItems() }
+
+            _viewState.value = WooPosItemsSearchViewState.EmptySearchQuery(
+                popularItems = popularItemsDeferred.await(),
+                recentSearches = lastSearchesDeferred.await(),
+            )
+        }
+    }
 }
