f6d7edc33f5341fe01aafcbd717944f50ce21df7
ThomazFB <thomazfcortez@gmail.com>
2025-03-17 19:41:34 -0300

    Define WooShippingLabelHazmatFormScreen main UI elements and behaviors

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormScreen.kt
@@ -1,15 +1,53 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.woocommerce.android.ui.compose.component.WCColoredButton
 
 @Composable
-fun WooShippingLabelHazmatFormScreen() {
+fun WooShippingLabelHazmatFormScreen(
+    containsHazmatChecked: Boolean,
+    onContainsHazmatChanged: (Boolean) -> Unit,
+    onSelectCategoryClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column {
-        Text("")
-        Row {  }
-        Text("")
+        Text("Are you shipping dangerous goods or hazardous materials?")
+        Row(
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Contains hazardous materials",
+                modifier = modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(1f)
+            )
+            Checkbox(
+                checked = containsHazmatChecked,
+                onCheckedChange = onContainsHazmatChanged,
+                colors = CheckboxDefaults.colors(
+                    checkedColor = MaterialTheme.colorScheme.primary,
+                    uncheckedColor = MaterialTheme.colorScheme.onSurface
+                )
+            )
+        }
+        WCColoredButton(
+            text = "Select Category",
+            onClick = onSelectCategoryClick,
+            enabled = containsHazmatChecked
+        )
+        HorizontalDivider()
+        Text("Hazmat details and info")
     }
 }
