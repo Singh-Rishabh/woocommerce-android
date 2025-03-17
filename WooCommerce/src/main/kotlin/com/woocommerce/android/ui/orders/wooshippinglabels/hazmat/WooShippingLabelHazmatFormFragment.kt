0933a9f69d467054bff97e8aa5b59df5abc60d29
ThomazFB <thomazfcortez@gmail.com>
2025-03-17 19:21:18 -0300

    Declare WooShippingLabelHazmatFormFragment with compose scaffold

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormFragment.kt
@@ -0,0 +1,25 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.woocommerce.android.ui.base.BaseFragment
+import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WooShippingLabelHazmatFormFragment : BaseFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WooThemeWithBackground {
+                    // Add Hazmat view here
+                }
+            }
+        }
+    }
+}
