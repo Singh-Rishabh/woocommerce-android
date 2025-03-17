4dec8f1eb180a178eef7d9b2fdfe97986b30bf48
ThomazFB <thomazfcortez@gmail.com>
2025-03-17 19:36:58 -0300

    Define WooShippingLabelHazmatFormScreen.kt

---
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormScreen.kt
@@ -0,0 +1,15 @@
+package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WooShippingLabelHazmatFormScreen() {
+    Column {
+        Text("")
+        Row {  }
+        Text("")
+    }
+}
