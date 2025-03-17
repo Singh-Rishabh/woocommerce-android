fab3fcd46cd0c345cd0d345b6cab1a840eeca8b7
ThomazFB <thomazfcortez@gmail.com>
2025-03-17 19:46:46 -0300

    Add ViewModel main interface with the Composable UI

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormViewModel.kt
@@ -2,14 +2,34 @@ package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
 
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.woocommerce.android.viewmodel.ScopedViewModel
+import com.woocommerce.android.viewmodel.getStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import kotlinx.parcelize.Parcelize
 
 @HiltViewModel
 class WooShippingLabelHazmatFormViewModel(
     savedState: SavedStateHandle
 ) : ScopedViewModel(savedState) {
+
+    private val _viewState = savedState.getStateFlow(
+        scope = viewModelScope,
+        initialValue = ViewState()
+    )
+    val viewState = _viewState.asLiveData()
+
+    fun onContainsHazmatChanged(containsHazmatChecked: Boolean) {
+        _viewState.update {
+            _viewState.value.copy(containsHazmatChecked = containsHazmatChecked)
+        }
+    }
+
+    fun onSelectCategoryClick() {
+
+    }
 
     @Parcelize
     data class ViewState(
