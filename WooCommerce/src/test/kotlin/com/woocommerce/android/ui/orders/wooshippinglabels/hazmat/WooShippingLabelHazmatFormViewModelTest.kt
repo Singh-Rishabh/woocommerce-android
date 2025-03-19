55d508d5f8e89ea07e01ace61db3a483889e72e1
ThomazFB <thomazfcortez@gmail.com>
2025-03-19 18:54:00 -0300

    Improve WooShippingLabelHazmatFormViewModelTest with the ViewModel changes

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
 
 import androidx.lifecycle.SavedStateHandle
+import com.woocommerce.android.ui.orders.shippinglabels.creation.ShippingLabelHazmatCategory
 import com.woocommerce.android.viewmodel.BaseUnitTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
@@ -17,7 +18,7 @@ class WooShippingLabelHazmatFormViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `when initialized, then containsHazmatChecked is false by default`() = testBlocking {
+    fun `when initialized, then viewState has default values`() = testBlocking {
         // Given/When
         var capturedViewState: WooShippingLabelHazmatFormViewModel.ViewState? = null
         viewModel.viewState.observeForever {
@@ -26,10 +27,11 @@ class WooShippingLabelHazmatFormViewModelTest : BaseUnitTest() {
 
         // Then
         assertThat(capturedViewState?.containsHazmatChecked).isFalse()
+        assertThat(capturedViewState?.currentHazmatSelection).isNull()
     }
 
     @Test
-    fun `when onContainsHazmatChanged is called with true, then viewState is updated accordingly`() = testBlocking {
+    fun `when onContainsHazmatChanged is called with true, then viewState is updated`() = testBlocking {
         // Given
         var capturedViewState: WooShippingLabelHazmatFormViewModel.ViewState? = null
         viewModel.viewState.observeForever {
@@ -44,7 +46,7 @@ class WooShippingLabelHazmatFormViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `when onContainsHazmatChanged is called with false, then viewState is updated accordingly`() = testBlocking {
+    fun `when onContainsHazmatChanged is called with false, then viewState is updated`() = testBlocking {
         // Given
         var capturedViewState: WooShippingLabelHazmatFormViewModel.ViewState? = null
         viewModel.viewState.observeForever {
@@ -75,7 +77,40 @@ class WooShippingLabelHazmatFormViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `when multiple calls to onContainsHazmatChanged, then viewState reflects latest value`() = testBlocking {
+    fun `when onUrlSelected is called, then OnUrlSelected event is triggered with correct URL`() = testBlocking {
+        // Given
+        val testUrl = "https://test.com"
+        var capturedEvent: WooShippingLabelHazmatFormViewModel.OnUrlSelected? = null
+        viewModel.event.observeForever {
+            capturedEvent = it as? WooShippingLabelHazmatFormViewModel.OnUrlSelected
+        }
+
+        // When
+        viewModel.onUrlSelected(testUrl)
+
+        // Then
+        assertThat(capturedEvent).isNotNull()
+        assertThat(capturedEvent?.url).isEqualTo(testUrl)
+    }
+
+    @Test
+    fun `when onHazmatCategorySelected is called, then viewState is updated with selected category`() = testBlocking {
+        // Given
+        val selectedCategory = ShippingLabelHazmatCategory.CLASS_1
+        var capturedViewState: WooShippingLabelHazmatFormViewModel.ViewState? = null
+        viewModel.viewState.observeForever {
+            capturedViewState = it
+        }
+
+        // When
+        viewModel.onHazmatCategorySelected(selectedCategory)
+
+        // Then
+        assertThat(capturedViewState?.currentHazmatSelection).isEqualTo(selectedCategory)
+    }
+
+    @Test
+    fun `when multiple state changes occur, then viewState reflects latest values`() = testBlocking {
         // Given
         var capturedViewState: WooShippingLabelHazmatFormViewModel.ViewState? = null
         viewModel.viewState.observeForever {
@@ -84,10 +119,12 @@ class WooShippingLabelHazmatFormViewModelTest : BaseUnitTest() {
 
         // When
         viewModel.onContainsHazmatChanged(true)
+        viewModel.onHazmatCategorySelected(ShippingLabelHazmatCategory.CLASS_1)
         viewModel.onContainsHazmatChanged(false)
-        viewModel.onContainsHazmatChanged(true)
+        viewModel.onHazmatCategorySelected(ShippingLabelHazmatCategory.CLASS_3)
 
         // Then
-        assertThat(capturedViewState?.containsHazmatChecked).isTrue()
+        assertThat(capturedViewState?.containsHazmatChecked).isFalse()
+        assertThat(capturedViewState?.currentHazmatSelection).isEqualTo(ShippingLabelHazmatCategory.CLASS_3)
     }
 }
