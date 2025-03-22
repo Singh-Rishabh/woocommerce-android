d10cc3230ece5780fdcfa751ac970ff1971dbcfe
Alejo <alejandro.torres.veiga@automattic.com>
2025-03-21 23:32:50 -0600

    delay instructions until the navigation transition is complete

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/split/WooShippingSplitShipmentViewModel.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/split/WooShippingSplitShipmentViewModel.kt
@@ -9,9 +9,11 @@ import com.woocommerce.android.viewmodel.MultiLiveEvent
 import com.woocommerce.android.viewmodel.ScopedViewModel
 import com.woocommerce.android.viewmodel.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.getValue
 
@@ -34,6 +36,13 @@ class WooShippingSplitShipmentViewModel @Inject constructor(
                 dimensionUnit = storeOptions.dimensionUnit,
                 weightUnit = storeOptions.weightUnit
             )
+        }
+    }
+
+    init {
+        launch {
+            delay(NOTIFICATIONS_DELAY)
+            splitMessage.value = SplitShipmentMessage.Instructions
         }
     }
 
@@ -63,6 +72,10 @@ class WooShippingSplitShipmentViewModel @Inject constructor(
         val hasChanges: Boolean = false,
         val splitMessage: SplitShipmentMessage? = null
     )
+
+    companion object {
+        const val NOTIFICATIONS_DELAY = 500L
+    }
 }
 
 data class SelectableShippableItemsUI(
