2d3ecf68a8968ddab0468495e741ad3c83c14852
ThomazFB <thomazfcortez@gmail.com>
2025-03-24 20:16:33 -0300

    Add Dark mode support and vertical scrolling to WooShippingLabelHazmatFormScreen

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/WooShippingLabelHazmatFormScreen.kt
@@ -1,11 +1,14 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -16,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +32,7 @@ import com.woocommerce.android.ui.compose.theme.WooThemeWithBackground
 fun WooShippingLabelHazmatFormScreen(viewModel: WooShippingLabelHazmatFormViewModel) {
     val viewState by viewModel.viewState.observeAsState()
     WooShippingLabelHazmatFormScreen(
-        containsHazmatChecked = viewState?.containsHazmatChecked ?: false,
+        containsHazmatChecked = viewState?.containsHazmatChecked == true,
         onContainsHazmatChanged = viewModel::onContainsHazmatChanged,
         onSelectCategoryClick = viewModel::onSelectCategoryClick
     )
@@ -42,24 +46,28 @@ fun WooShippingLabelHazmatFormScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = stringResource(R.string.woo_shipping_labels_hazmat_info_title),
             style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.color_on_surface_medium)
         )
         Row(
             horizontalArrangement = Arrangement.Absolute.SpaceBetween,
             modifier = modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp)
+                .padding(vertical = 8.dp)
         ) {
             Text(
                 text = stringResource(R.string.woo_shipping_labels_hazmat_info_contains_hazmat),
                 style = MaterialTheme.typography.bodyLarge,
+                color = colorResource(id = R.color.color_on_surface_medium),
                 modifier = modifier
                     .align(Alignment.CenterVertically)
                     .weight(1f)
@@ -68,8 +76,8 @@ fun WooShippingLabelHazmatFormScreen(
                 checked = containsHazmatChecked,
                 onCheckedChange = onContainsHazmatChanged,
                 colors = CheckboxDefaults.colors(
-                    checkedColor = MaterialTheme.colorScheme.primary,
-                    uncheckedColor = MaterialTheme.colorScheme.onSurface
+                    checkedColor = MaterialTheme.colorScheme.onPrimary,
+                    uncheckedColor = colorResource(id = R.color.color_on_surface_disabled),
                 )
             )
         }
@@ -80,16 +88,18 @@ fun WooShippingLabelHazmatFormScreen(
             modifier = modifier.fillMaxWidth()
         )
 
-        HorizontalDivider(modifier = modifier.padding(vertical = 16.dp))
+        HorizontalDivider(modifier = modifier.padding(vertical = 8.dp))
 
         Text(
             text = stringResource(R.string.woo_shipping_labels_hazmat_info_full_description),
-            style = MaterialTheme.typography.bodyMedium
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorResource(id = R.color.color_on_surface_medium),
         )
     }
 }
 
 @Preview
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun WooShippingLabelHazmatFormScreenPreview() {
     WooThemeWithBackground {
