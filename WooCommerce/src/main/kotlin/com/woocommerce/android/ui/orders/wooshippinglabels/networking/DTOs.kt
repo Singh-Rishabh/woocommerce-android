046d1ee70b01e604ba9a86fe22c08c24f2954f6f
ThomazFB <thomazfcortez@gmail.com>
2025-03-26 09:10:12 -0300

    Update CustomsDTO to correctly set the isReturnToSender value

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/DTOs.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/networking/DTOs.kt
@@ -119,7 +119,7 @@ data class CustomsDTO(
     @SerializedName("contents_explanation") val contentExplanation: String,
     @SerializedName("restriction_type") val restrictionType: String,
     @SerializedName("restriction_comments") val restrictionComments: String,
-    @SerializedName("non_delivery_option") val isReturnToSender: Boolean,
+    @SerializedName("non_delivery_option") val isReturnToSender: String,
     val itn: String,
     val items: List<CustomsItemDTO>
 )
