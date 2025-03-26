a27f765c06f7522be52e46bd22ecd85d1b5ab03f
ThomazFB <thomazfcortez@gmail.com>
2025-03-25 21:46:43 -0300

    Add spacer between selected Hazmat category and Hazmat CTA

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/WooShippingLabelCreationScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -536,7 +538,7 @@ internal fun HazmatCard(
         } else {
             colorResource(id = R.color.woo_gray_5)
         }
-
+        Spacer(modifier = Modifier.height(4.dp))
         Box(modifier = Modifier.padding(horizontal = 16.dp)) {
             Box(
                 modifier = Modifier
