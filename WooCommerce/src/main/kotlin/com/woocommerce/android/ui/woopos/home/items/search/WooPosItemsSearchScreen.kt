21d67b32ef5eb6e0b1bee00b868a339856e5f7ac
Andrey <akidinov@gmail.com>
2025-03-26 18:44:06 +0100

    WooPosItemList with named params

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/home/items/search/WooPosItemsSearchScreen.kt
@@ -67,20 +67,21 @@ private fun WooPosItemsSearchContent(
 ) {
     val listState = rememberLazyListState()
     WooPosItemList(
-        state,
-        listState,
-        { onUIEvent(WooPosItemsSearchUiEvent.ItemClicked(it)) },
-        { onUIEvent(WooPosItemsSearchUiEvent.EndOfItemsListReached) },
-    ) {
-        WooPosPaginationErrorIndicator(
-            message = stringResource(id = R.string.woopos_items_pagination_error_title),
-            description = stringResource(id = R.string.woopos_items_pagination_error_description),
-            primaryButton = Button(
-                text = stringResource(id = R.string.woopos_items_pagination_try_again_label),
-                click = { onUIEvent(WooPosItemsSearchUiEvent.EndOfItemsListReached) }
-            ),
-        )
-    }
+        state = state,
+        listState = listState,
+        onItemClicked = { onUIEvent(WooPosItemsSearchUiEvent.ItemClicked(it)) },
+        onEndOfProductsListReached = { onUIEvent(WooPosItemsSearchUiEvent.EndOfItemsListReached) },
+        onErrorWhilePaginating = {
+            WooPosPaginationErrorIndicator(
+                message = stringResource(id = R.string.woopos_items_pagination_error_title),
+                description = stringResource(id = R.string.woopos_items_pagination_error_description),
+                primaryButton = Button(
+                    text = stringResource(id = R.string.woopos_items_pagination_try_again_label),
+                    click = { onUIEvent(WooPosItemsSearchUiEvent.EndOfItemsListReached) }
+                ),
+            )
+        }
+    )
 }
 
 @Composable
