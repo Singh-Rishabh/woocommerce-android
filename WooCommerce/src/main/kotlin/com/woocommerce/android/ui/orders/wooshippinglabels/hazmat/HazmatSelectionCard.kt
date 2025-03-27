ebab5778cf9387cd7ddfa7021e03f2dcdd1cd858
ThomazFB <thomazfcortez@gmail.com>
2025-03-27 19:56:23 -0300

    Update HazmatSelectionCard text color for better dark mode support

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/HazmatSelectionCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/HazmatSelectionCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,7 +41,8 @@ fun HazmatSelectionCard(
                 .padding(16.dp)
         ) {
             Text(
-                text = stringResource(selectedCategory.stringResourceID)
+                text = stringResource(selectedCategory.stringResourceID),
+                color = colorResource(id = R.color.color_on_surface),
             )
         }
     }
