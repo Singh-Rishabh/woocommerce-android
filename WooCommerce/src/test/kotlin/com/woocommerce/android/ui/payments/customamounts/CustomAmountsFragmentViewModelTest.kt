038fd89521986c8c90209647f0d6d0b214fef524
AnirudhBhat <abhat38@gmail.com>
2025-04-01 10:33:36 +0530

    Remove unused imports

--- WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/customamounts/CustomAmountsFragmentViewModelTest.kt
+++ WooCommerce/src/test/kotlin/com/woocommerce/android/ui/payments/customamounts/CustomAmountsFragmentViewModelTest.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
