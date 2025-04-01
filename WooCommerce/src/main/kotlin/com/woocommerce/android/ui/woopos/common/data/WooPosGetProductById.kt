2c409d3c691317e67c36ffcb38ab2ac13b0155c6
samiuelson <samiuelson@gmail.com>
2025-04-01 17:31:48 +0200

    Remove `WooPosSearchProductsMockedDataSource` from `WooPosGetProductById`

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/woopos/common/data/WooPosGetProductById.kt
@@ -4,7 +4,6 @@ import com.woocommerce.android.model.Product
 import com.woocommerce.android.model.toAppModel
 import com.woocommerce.android.tools.SelectedSite
 import com.woocommerce.android.ui.woopos.featureflags.WooPosIsProductsSearchEnabled
-import com.woocommerce.android.ui.woopos.home.items.search.WooPosSearchProductsMockedDataSource
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.store.WCProductStore
@@ -14,12 +13,11 @@ class WooPosGetProductById @Inject constructor(
     private val store: WCProductStore,
     private val site: SelectedSite,
     private val isWooPosSearchEnabled: WooPosIsProductsSearchEnabled,
-    private val searchProductsMockedDataSource: WooPosSearchProductsMockedDataSource,
 ) {
     suspend operator fun invoke(productId: Long): Product? = withContext(IO) {
         val result = store.getProductByRemoteId(site.getOrNull()!!, productId)?.toAppModel()
         if (result == null && isWooPosSearchEnabled()) {
-            searchProductsMockedDataSource.getProductById(productId)
+            store.getProductByRemoteId(site.get(), productId)?.toAppModel()
         } else {
             result
         }
