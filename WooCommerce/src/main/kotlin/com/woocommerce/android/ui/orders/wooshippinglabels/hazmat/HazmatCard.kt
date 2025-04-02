2031d7cb5eef303508bf92825fd8323482c2da97
ThomazFB <thomazfcortez@gmail.com>
2025-04-02 07:32:19 -0300

    Overhaul the HazmatCard padding instructions

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/HazmatCard.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/orders/wooshippinglabels/hazmat/HazmatCard.kt
@@ -1,10 +1,12 @@
 package com.woocommerce.android.ui.orders.wooshippinglabels.hazmat
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +15,8 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.woocommerce.android.R
 import com.woocommerce.android.ui.orders.shippinglabels.creation.ShippingLabelHazmatCategory
 
@@ -24,48 +28,50 @@ internal fun HazmatCard(
     selectedCategory: ShippingLabelHazmatCategory? = null,
     onClick: OnClick? = null
 ) {
-    var hazmatCardModifier = modifier
-    if (onClick != null) {
-        hazmatCardModifier = modifier.then(modifier.clickable { onClick() })
-    }
+    Column {
+        val mainRowModifier = if (onClick != null) {
+            Modifier.then(modifier.clickable { onClick() })
+        } else {
+            modifier
+        }
 
-    Row(modifier = hazmatCardModifier) {
-        Text(
-            text = stringResource(R.string.shipping_label_hazmat_title),
-            style = MaterialTheme.typography.subtitle1,
-            color = MaterialTheme.colors.onSurface,
-            modifier = Modifier
-                .weight(1f)
-                .padding(dimensionResource(id = R.dimen.major_100))
-                .align(Alignment.CenterVertically)
-        )
+        Row(modifier = mainRowModifier) {
+            Text(
+                text = stringResource(R.string.shipping_label_hazmat_title),
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onSurface,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = dimensionResource(id = R.dimen.major_100))
+                    .align(Alignment.CenterVertically)
+            )
 
-        Text(
-            text = if (selectedCategory == null) stringResource(R.string.no) else stringResource(R.string.yes),
-            style = MaterialTheme.typography.subtitle1,
-            color = colorResource(id = R.color.color_on_surface_medium),
-            modifier = Modifier
-                .align(Alignment.CenterVertically)
-        )
-
-        if (onClick != null) {
-            Icon(
-                painter = painterResource(R.drawable.ic_arrow_right),
-                tint = colorResource(id = R.color.color_on_surface_medium),
-                contentDescription = stringResource(
-                    id = R.string.shipping_label_package_details_items_expand_content_description
-                ),
+            Text(
+                text = if (selectedCategory == null) stringResource(R.string.no) else stringResource(R.string.yes),
+                style = MaterialTheme.typography.subtitle1,
+                color = colorResource(id = R.color.color_on_surface_medium),
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .padding(end = dimensionResource(R.dimen.minor_50))
+            )
+
+            if (onClick != null) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_right),
+                    tint = colorResource(id = R.color.color_on_surface_medium),
+                    contentDescription = stringResource(
+                        id = R.string.shipping_label_package_details_items_expand_content_description
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                )
+            }
+        }
+
+        if (selectedCategory != null) {
+            HazmatSelectionCard(
+                selectedCategory = selectedCategory,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.major_100))
             )
         }
-    }
-
-    if (selectedCategory != null) {
-        HazmatSelectionCard(
-            selectedCategory = selectedCategory,
-            modifier = Modifier.padding(dimensionResource(id = R.dimen.major_100))
-        )
     }
 }
