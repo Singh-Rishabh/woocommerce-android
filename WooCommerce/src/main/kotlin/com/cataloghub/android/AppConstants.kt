17b3e09a172ec70fe9ca811423e586a2c30832df
Singh-Rishabh <singhrishabh.cs@gmail.com>
2025-04-05 15:25:24 +0530

    Fixed social links Fixed terms and condition.

--- WooCommerce/src/main/kotlin/com/cataloghub/android/AppConstants.kt
+++ WooCommerce/src/main/kotlin/com/cataloghub/android/AppConstants.kt
@@ -6,7 +6,7 @@ package com.cataloghub.android
 object AppConstants {
     const val REQUEST_TIMEOUT = 40L * 1000
     const val SEARCH_TYPING_DELAY_MS = 500L
-    const val X_USERNAME = "cataloghub"
-    const val INSTAGRAM_USERNAME = "cataloghub"
+    const val YOUTUBE_HANDLE = "cataloghub.in.official"
+    const val INSTAGRAM_USERNAME = "cataloghub_"
     const val FADE_ANIMATION_DELAY_MS = 1000
 }
